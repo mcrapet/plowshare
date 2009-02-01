85037fd24a195bdb5cdb6ce087a19ac4b6f7db99
Arnau Sanchez <pyarnau@gmail.com>
2009-02-01 12:58:17 +0000

    add upload function
    git-svn-id: https://plowshare.googlecode.com/svn/trunk@41 51fab9ec-ea3e-11dd-80ed-bda8ba2650cd

--- src/modules/2shared.sh
+++ src/modules/2shared.sh
@@ -14,3 +14,24 @@ MODULE_2SHARED_REGEXP_URL="http://\(www\.\)\?2shared.com/file/"
     test "$FILE_URL" || { debug "file not found"; return 1; }
     echo $FILE_URL    
 }
+
+# Upload a file to 2shared
+#
+# $1: File path
+#
+2shared_upload() {
+    FILE=$1
+    UPLOADURL="http://www.2shared.com/"
+
+    debug "downloading upload page: $UPLOADURL"
+    DATA=$(curl "$UPLOADURL")
+    ACTION=$(echo "$DATA" | parse "uploadForm" 'action="\([^"]*\)"')
+    COMPLETE=$(echo "$DATA" | parse "uploadComplete" 'location="\([^"]*\)"')
+    debug "starting file upload: $FILE"
+    STATUS=$(curl -F "mainDC=1" \
+        -F "fff=@$FILE;filename=$(basename "$FILE")" \
+        "$ACTION")
+    match "upload has successfully completed" "$STATUS" ||
+        { debug "error on upload"; return 1; }
+    curl "$UPLOADURL/$COMPLETE" | parse 'name="downloadLink"' "\(http:[^<]*\)"
+}
