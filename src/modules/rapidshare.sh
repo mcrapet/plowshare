a7381cc8064fec7c9850e53b08e68bfb20587d9a
Arnau Sanchez <pyarnau@gmail.com>
2009-02-17 23:28:22 +0000

    loop if this IP is downloading a file
    git-svn-id: https://plowshare.googlecode.com/svn/trunk@62 51fab9ec-ea3e-11dd-80ed-bda8ba2650cd

--- src/modules/rapidshare.sh
+++ src/modules/rapidshare.sh
@@ -13,25 +13,28 @@ MODULE_RAPIDSHARE_UPLOAD_OPTIONS="a:,auth-freezone:,AUTH_FREEZONE,USER:PASSWORD"
 # rapidshare_download RAPIDSHARE_URL
 #
 rapidshare_download() {
+    set -e
     URL=$1
     while true; do
         WAIT_URL=$(curl "$URL" | parse '<form' 'action="\(.*\)"') ||
             { debug "file not found"; return 1; }
         DATA=$(curl --data "dl.start=Free" "$WAIT_URL") ||
             { debug "can't get wait URL contents"; return 1; }
-        LIMIT=$(echo "$DATA" | parse "try again" "\([[:digit:]]\+\) minutes")
+        if echo "$DATA" | grep -o "Your IP address.*file" >&2; then
+            debug "Sleeping 1 minute and trying again"
+            sleep 60
+            continue
+        fi
+        LIMIT=$(echo "$DATA" | \
+            parse "try again" "[[:space:]]\([[:digit:]]\+\) minutes" || true)
         test -z "$LIMIT" && break
         debug "download limit reached: waiting $LIMIT minutes"
         sleep $((LIMIT*60))
     done
-    DOWNLOADING=$(echo "$DATA" | grep -o "Your IP address.*file") &&
-        { debug "$DOWNLOADING"; return 2; }
-    FILE_URL=$(echo "$DATA" | parse "<form " 'action="\([^\"]*\)"') ||
-        { debug "can't get file URL"; return 1; } 
-    SLEEP=$(echo "$DATA" | parse "^var c=" "c=\([[:digit:]]\+\);") ||
-        { debug "can't get sleep time"; SLEEP=100; }
+    FILE_URL=$(echo "$DATA" | parse "<form " 'action="\([^\"]*\)"') 
+    SLEEP=$(echo "$DATA" | parse "^var c=" "c=\([[:digit:]]\+\);")
     debug "URL File: $FILE_URL" 
-    debug "Waiting $SLEEP seconds" 
+    debug "waiting $SLEEP seconds" 
     sleep $(($SLEEP + 1))
     echo $FILE_URL    
 }
@@ -44,6 +47,7 @@ rapidshare_download() {
 #   -a USER:PASSWORD, --auth=USER:PASSWORD
 #
 rapidshare_upload() {
+    set -e
     eval "$(process_options "$MODULE_RAPIDSHARE_UPLOAD_OPTIONS" "$@")"    
     if [ "$AUTH_FREEZONE" ]; then
         rapidshare_upload_freezone "$@"
@@ -54,22 +58,18 @@ rapidshare_upload() {
 
 # Upload a file to Rapidshare anonymously
 #
-# rapidshare_upload [OPTIONS] FILE
-#
+# rapidshare_upload FILE
 #
 rapidshare_upload_anonymous() {
+    set -e
     FILE=$1
     UPLOAD_URL="http://www.rapidshare.com"
     debug "downloading upload page: $UPLOAD_URL"
-    ACTION=$(curl "$UPLOAD_URL" | parse 'form name="ul"' 'action="\([^"]*\)') ||
-        { debug "can't get upload action url"; return 2; }
+    ACTION=$(curl "$UPLOAD_URL" | parse 'form name="ul"' 'action="\([^"]*\)')
     debug "upload to: $ACTION"    
-    INFO=$(curl -F "filecontent=@$FILE;filename=$(basename "$FILE")" "$ACTION") ||      
-        { debug "can't upload file"; return 2; }
-    URL=$(echo "$INFO" | parse "downloadlink" ">\(.*\)<") ||
-        { debug "can't get download link"; return 2; }        
-    KILL=$(echo "$INFO" | parse "loeschlink" ">\(.*\)<") ||
-        { debug "can't get kill link"; return 2; }     
+    INFO=$(curl -F "filecontent=@$FILE;filename=$(basename "$FILE")" "$ACTION")
+    URL=$(echo "$INFO" | parse "downloadlink" ">\(.*\)<")     
+    KILL=$(echo "$INFO" | parse "loeschlink" ">\(.*\)<")
     echo "$URL ($KILL)"
 }
 
@@ -81,15 +81,14 @@ rapidshare_upload_anonymous() {
 #   -a USER:PASSWORD, --auth=USER:PASSWORD
 #
 rapidshare_upload_freezone() {
+    set -e
     eval "$(process_options "$MODULE_RAPIDSHARE_UPLOAD_OPTIONS" "$@")"
     FILE=$1
     FREEZONE_LOGIN_URL="https://ssl.rapidshare.com/cgi-bin/collectorszone.cgi"
        
-    if [ "$AUTH_FREEZONE" ]; then 
-        COOKIES=$(post_login "username" "password" \
-            "$AUTH_FREEZONE" "$FREEZONE_LOGIN_URL")
-        test "$COOKIES" || { debug "error on login process"; return 1; }
-    fi
+    COOKIES=$(post_login "username" "password" \
+        "$AUTH_FREEZONE" "$FREEZONE_LOGIN_URL")
+    test "$COOKIES" || { debug "error on login process"; return 1; }
     ccurl() { curl -b <(echo "$COOKIES") "$@"; }
     debug "downloading upload page: $UPLOAD_URL"
     UPLOAD_PAGE=$(ccurl $FREEZONE_LOGIN_URL)
@@ -108,8 +107,8 @@ rapidshare_upload_freezone() {
     FILEID=$(echo "$UPLOAD_PAGE" | grep ^Adliste | tail -n1 | \
         parse Adliste 'Adliste\["\([[:digit:]]*\)"')
     MATCH="^Adliste\[\"$FILEID\"\]"
-    KILLCODE=$(echo "$UPLOAD_PAGE" | parse "$MATCH" "\[\"killcode\"\] = '\(.*\)'")
-    FILENAME=$(echo "$UPLOAD_PAGE" | parse "$MATCH" "\[\"filename\"\] = \"\(.*\)\"")
+    KILLCODE=$(echo "$UPLOAD_PAGE" | parse "$MATCH" "killcode\"\] = '\(.*\)'")
+    FILENAME=$(echo "$UPLOAD_PAGE" | parse "$MATCH" "filename\"\] = \"\(.*\)\"")
     # There is a killcode in the HTML, but it's not used to build a URL
     # but as a param in a POST, so I assume there is no kill URL for
     # freezone. Therefore, output only the file URL.    
