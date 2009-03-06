08ba71704e72b74e37a7ac8a5f035557867fd822
Arnau Sanchez <pyarnau@gmail.com>
2009-03-06 22:32:50 +0000

    make bin links relative to share
    git-svn-id: https://plowshare.googlecode.com/svn/trunk@205 51fab9ec-ea3e-11dd-80ed-bda8ba2650cd

--- setup.sh
+++ setup.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+#
+# This file is part of Foobar.
+#
+# Foobar is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Foobar is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+#
 set -e
 
 NAME=plowshare
@@ -8,6 +24,7 @@ LIBDIR=$USRDIR/share/$NAME
 BINDIR=$USRDIR/bin
 DOCSDIR=$USRDIR/share/doc/$NAME
 MODULESDIR=$LIBDIR/modules
+BIN2LIB=../share/$NAME
 USAGE="Usage: setup.sh install|uninstall"
 
 test $# -eq 0 && { echo "$USAGE"; exit 1; }
@@ -35,8 +52,8 @@ elif [ "$1" = "install" ]; then
 
     # Binary files
     mkdir -p $BINDIR 
-    ln -vsf $(readlink -f $LIBDIR/download.sh) $BINDIR/plowdown
-    ln -vsf $(readlink -f $LIBDIR/upload.sh) $BINDIR/plowup
+    ln -vsf $BIN2LIB/download.sh $BINDIR/plowdown
+    ln -vsf $BIN2LIB/upload.sh $BINDIR/plowup
 else
     echo "$USAGE"
     exit 1
