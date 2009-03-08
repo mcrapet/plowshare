06734c5e895060424d2da7de797a9818e6f9e41a
Arnau Sanchez <pyarnau@gmail.com>
2009-03-08 14:28:03 +0000

    enhanced test for setup script add mediafire functional test
    git-svn-id: https://plowshare.googlecode.com/svn/trunk@209 51fab9ec-ea3e-11dd-80ed-bda8ba2650cd

--- test/test_functional.sh
+++ test/test_functional.sh
@@ -152,13 +152,58 @@ test_badongo_download() {
 
 ### Setup script
 
+INSTALLED="usr
+usr/local
+usr/local/bin
+usr/local/bin/plowdown
+usr/local/bin/plowup
+usr/local/share
+usr/local/share/doc
+usr/local/share/doc/plowshare
+usr/local/share/doc/plowshare/CHANGELOG
+usr/local/share/doc/plowshare/COPYING
+usr/local/share/doc/plowshare/README
+usr/local/share/plowshare
+usr/local/share/plowshare/download.sh
+usr/local/share/plowshare/lib.sh
+usr/local/share/plowshare/modules
+usr/local/share/plowshare/modules/2shared.sh
+usr/local/share/plowshare/modules/badongo.sh
+usr/local/share/plowshare/modules/extras
+usr/local/share/plowshare/modules/extras/megaupload_captcha.py
+usr/local/share/plowshare/modules/extras/news_gothic_bt.ttf
+usr/local/share/plowshare/modules/mediafire.sh
+usr/local/share/plowshare/modules/megaupload.sh
+usr/local/share/plowshare/modules/rapidshare.sh
+usr/local/share/plowshare/upload.sh"
+
+UNINSTALLED="usr
+usr/local
+usr/local/bin
+usr/local/share
+usr/local/share/doc"
+
 test_setup_script() {
     TEMPDIR=$(mktemp -d)
     assert_return 0 "DESTDIR=$TEMPDIR $ROOTDIR/setup.sh install"
-    assert_equal 22 $(find $TEMPDIR | wc -l)
+    assert_equal "$INSTALLED" \
+        "$(find $TEMPDIR | sed "s#^$TEMPDIR/\?##" | grep -v "^$" | sort)"
     assert_return 0 "DESTDIR=$TEMPDIR $ROOTDIR/setup.sh uninstall"
-    assert_equal 4 $(find $TEMPDIR | wc -l)
+    assert_equal "$UNINSTALLED" \
+        "$(find $TEMPDIR | sed "s#^$TEMPDIR/\?##" | grep -v "^$" | sort)"
     rm -rf $TEMPDIR
 }
 
+#
+
+## Badongo
+
+MEDIAFIRE_URL="http://www.mediafire.com/?mokvnz2y43y"
+
+test_mediafire_download() {
+    FILENAME="Nature+Medicine.pdf"
+    assert_equal "$FILENAME" "$(download $MEDIAFIRE_URL)"
+    rm -f $FILENAME
+}        
+ 
 run_tests "$@"
