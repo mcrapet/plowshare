7713ce8157cbe52517b09b1381473de7a5e3bf3e
Arnau Sanchez <pyarnau@gmail.com>
2009-03-20 23:32:06 +0000

    fix setup: delargo not used fix setup test
    git-svn-id: https://plowshare.googlecode.com/svn/trunk@274 51fab9ec-ea3e-11dd-80ed-bda8ba2650cd

--- test/test_functional.sh
+++ test/test_functional.sh
@@ -25,6 +25,49 @@ upload() {
 
 UPFILE="/etc/services"
 
+### Setup script
+
+INSTALLED="usr
+usr/local
+usr/local/bin
+usr/local/bin/plowdown
+usr/local/bin/plowup
+usr/local/share
+usr/local/share/doc
+usr/local/share/doc/plowshare
+usr/local/share/doc/plowshare/CHANGELOG
+usr/local/share/doc/plowshare/COPYING
+usr/local/share/doc/plowshare/README
+usr/local/share/plowshare
+usr/local/share/plowshare/download.sh
+usr/local/share/plowshare/lib.sh
+usr/local/share/plowshare/modules
+usr/local/share/plowshare/modules/2shared.sh
+usr/local/share/plowshare/modules/badongo.sh
+usr/local/share/plowshare/modules/extras
+usr/local/share/plowshare/modules/extras/megaupload_captcha.py
+usr/local/share/plowshare/modules/mediafire.sh
+usr/local/share/plowshare/modules/megaupload.sh
+usr/local/share/plowshare/modules/rapidshare.sh
+usr/local/share/plowshare/upload.sh"
+
+UNINSTALLED="usr
+usr/local
+usr/local/bin
+usr/local/share
+usr/local/share/doc"
+
+test_00setup_script() {
+    TEMPDIR=$(mktemp -d)
+    assert_return 0 "DESTDIR=$TEMPDIR $ROOTDIR/setup.sh install"
+    assert_equal "$INSTALLED" \
+        "$(find $TEMPDIR | sed "s#^$TEMPDIR/\?##" | grep -v "^$" | sort)"
+    assert_return 0 "DESTDIR=$TEMPDIR $ROOTDIR/setup.sh uninstall"
+    assert_equal "$UNINSTALLED" \
+        "$(find $TEMPDIR | sed "s#^$TEMPDIR/\?##" | grep -v "^$" | sort)"
+    rm -rf $TEMPDIR
+}
+
 ## Rapidshare
 
 RAPIDSHARE_URL="http://www.rapidshare.com/files/86545320/Tux-Trainer_25-01-2008.rar"
@@ -143,52 +186,6 @@ test_badongo_download() {
     assert_equal "$FILENAME" "$(download $BADONGO_URL)"
     rm -f $FILENAME
 }        
-
-### Setup script
-
-INSTALLED="usr
-usr/local
-usr/local/bin
-usr/local/bin/plowdown
-usr/local/bin/plowup
-usr/local/share
-usr/local/share/doc
-usr/local/share/doc/plowshare
-usr/local/share/doc/plowshare/CHANGELOG
-usr/local/share/doc/plowshare/COPYING
-usr/local/share/doc/plowshare/README
-usr/local/share/plowshare
-usr/local/share/plowshare/download.sh
-usr/local/share/plowshare/lib.sh
-usr/local/share/plowshare/modules
-usr/local/share/plowshare/modules/2shared.sh
-usr/local/share/plowshare/modules/badongo.sh
-usr/local/share/plowshare/modules/extras
-usr/local/share/plowshare/modules/extras/delargo_dt_black.png
-usr/local/share/plowshare/modules/extras/megaupload_captcha.py
-usr/local/share/plowshare/modules/mediafire.sh
-usr/local/share/plowshare/modules/megaupload.sh
-usr/local/share/plowshare/modules/rapidshare.sh
-usr/local/share/plowshare/upload.sh"
-
-UNINSTALLED="usr
-usr/local
-usr/local/bin
-usr/local/share
-usr/local/share/doc"
-
-test_setup_script() {
-    TEMPDIR=$(mktemp -d)
-    assert_return 0 "DESTDIR=$TEMPDIR $ROOTDIR/setup.sh install"
-    assert_equal "$INSTALLED" \
-        "$(find $TEMPDIR | sed "s#^$TEMPDIR/\?##" | grep -v "^$" | sort)"
-    assert_return 0 "DESTDIR=$TEMPDIR $ROOTDIR/setup.sh uninstall"
-    assert_equal "$UNINSTALLED" \
-        "$(find $TEMPDIR | sed "s#^$TEMPDIR/\?##" | grep -v "^$" | sort)"
-    rm -rf $TEMPDIR
-}
-
-#
 
 ## Badongo
 
