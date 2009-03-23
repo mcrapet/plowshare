65e5067d01d8513bc49e9317dffd07e8d1f8d5af
Arnau Sanchez <pyarnau@gmail.com>
2009-03-23 23:01:25 +0000

    create tests log
    git-svn-id: https://plowshare.googlecode.com/svn/trunk@307 51fab9ec-ea3e-11dd-80ed-bda8ba2650cd

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,51 @@
 #!/bin/bash
-TESTSDIR=$(dirname "$(readlink -f "$0")")/test
+set -e
 
+TESTSDIR=$(dirname "$(readlink -f "$0")")/test
+PACKAGES="
+bash, --version, 4
+sed, --version, NF
+curl, --version, 2
+recode, --version, 3
+convert, --version, 3
+smjs, --version, 2
+tesseract, zcat /usr/share/doc/tesseract-ocr/changelog.gz | grep ' - V' | tail -n1 | awk '{print \$5}' | sed 's/^V//'
+python, --version, 2
+python-imaging, python -c 'import PIL.Image as i; print i.VERSION'
+aview, --version, 5
+"
+
+basic_info() {
+    VERSION=$(cat CHANGELOG | head -n1 | sed "s/^.*(\(.*\)).*$/\1/")
+    echo "plowshare: $VERSION"
+    echo "kernel: $(uname -a)"
+}
+
+version_info() {
+    while read LINE; do
+        IFS="," read APP ARGS FIELD <<< "$LINE"
+        test "$APP" || continue
+        if test "$FIELD"; then
+            VERSION=$($APP $ARGS 2>&1 | head -n1 | awk "{print \$$FIELD}")
+        else
+            VERSION=$(bash -c "$ARGS")
+        fi
+        echo "$APP: $VERSION"
+    done <<< "$1"
+}
+
+
+basic_info
+echo
+echo "--- Packages info"
+version_info "$PACKAGES"
+echo
+echo "--- Setup test"
+$TESTSDIR/test_setup.sh
+echo
+echo "--- Library tests"
 $TESTSDIR/test_lib.sh
-$TESTSDIR/test_functional.sh
+echo "--- Modules tests"
+echo
+$TESTSDIR/test_modules.sh
+echo done
