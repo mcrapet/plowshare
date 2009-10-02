82cf8de99568cec5fff7a8dfbc3ee107f0b304f1
Arnau Sanchez <pyarnau@gmail.com>
2009-10-02 06:31:58 +0000

    add MU (processed) image for OCR test
    git-svn-id: https://plowshare.googlecode.com/svn/trunk@487 51fab9ec-ea3e-11dd-80ed-bda8ba2650cd

--- test/test_lib.sh
+++ test/test_lib.sh
@@ -36,6 +36,7 @@ test_match() {
 test_ocr() {
     assert_equal "Hello world" "$(ocr < $PICSDIR/hello_world.gif 2>/dev/null)"
     assert_equal "XGXD" "$(ocr < $PICSDIR/badongo_xgxd.gif 2>/dev/null)"
+    assert_equal "DTE5" "$(ocr < $PICSDIR/megaupload_dte5.gif 2>/dev/null)"
 }
 
 test_check_exec() {
