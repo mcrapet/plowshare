99c70f7207ba84c901b3a9ff419dc9f68294a618
Matthieu Crapet <mcrapet@gmail.com>
2010-01-02 11:44:29 +0000

    strip_single_color.pl: added plowshare copyright header
    git-svn-id: https://plowshare.googlecode.com/svn/trunk@600 51fab9ec-ea3e-11dd-80ed-bda8ba2650cd

--- src/strip_single_color.pl
+++ src/strip_single_color.pl
@@ -1,10 +1,24 @@
 #!/bin/perl
-##
+#
+# This file is part of Plowshare.
+#
+# Plowshare is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Plowshare is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Plowshare.  If not, see <http://www.gnu.org/licenses/>.
+#
 # Delete (turn into white) each pixel which have a unique color inside the whole image.
 #
 # Usage: $0 [input-file] [output-file]
 # Debian users: apt-get install perlmagick
-##
 
 use Image::Magick;
 
@@ -28,7 +42,7 @@ my @histogram = $image->Histogram();
 while (@histogram) {
 	my($red, $green, $blue, $opacity, $count) = splice(@histogram, 0, 5);
 
-        # example: "43433,16962,49858,0" (r,g,b,a)
+	# example: "43433,16962,49858,0" (r,g,b,a)
 	push(@uniqcolors, join(',', ($red, $green, $blue, $opacity))) if ($count == 1);
 }
 
