1f2c48ce64a35db6d3dbb2c3c8ea4bc6b2e5b6c4
Arnau Sanchez <pyarnau@gmail.com>
2010-01-04 00:51:11 +0000

    add 2 new modules
    git-svn-id: https://plowshare.googlecode.com/svn/trunk@613 51fab9ec-ea3e-11dd-80ed-bda8ba2650cd

---
+++ src/modules/sendspace.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#
+# This file is part of Plowshare.
+#
+# Plowshare is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Plowshare is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Plowshare.  If not, see <http://www.gnu.org/licenses/>.
+#
+MODULE_SENDSPACE_REGEXP_URL="http://\(www\.\)\?sendspace.com/file/"
+MODULE_SENDSPACE_DOWNLOAD_OPTIONS="
+CHECK_LINK,c,check-link,,Check if a link exists and return"
+MODULE_SENDSPACE_UPLOAD_OPTIONS=
+MODULE_SENDSPACE_DOWNLOAD_CONTINUE=no
+
+# Output a sendspace file download URL
+#
+# $1: A sendspace URL
+#
+sendspace_download() {
+    set -e
+    eval "$(process_options sendspace "$MODULE_SENDSPACE_DOWNLOAD_OPTIONS" "$@")"
+    URL=$1
+
+    FILE_URL=$(curl -L --data "download=1" "$URL" | 
+        parse 'spn_download_link' 'href="\([^"]*\)"') || 
+        { error "file not found"; return 1; }
+
+    test "$CHECK_LINK" && return 255
+    HOST=$(echo "$FILE_URL" | grep -o "^http://[^/]*")
+    PATH=$(curl -I "$FILE_URL" | grep_http_header_location) || return 1
+    echo ${HOST}${PATH}
+}
---
+++ src/modules/usershare.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#
+# This file is part of Plowshare.
+#
+# Plowshare is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Plowshare is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Plowshare.  If not, see <http://www.gnu.org/licenses/>.
+#
+MODULE_USERSHARE_REGEXP_URL="http://\(www\.\)\?usershare.net/"
+MODULE_USERSHARE_DOWNLOAD_OPTIONS="
+CHECK_LINK,c,check-link,,Check if a link exists and return"
+MODULE_USERSHARE_UPLOAD_OPTIONS=
+MODULE_USERSHARE_DOWNLOAD_CONTINUE=no
+
+# Output a usershare file download URL
+#
+# $1: A usershare URL
+#
+usershare_download() {
+    set -e
+    eval "$(process_options usershare "$MODULE_USERSHARE_DOWNLOAD_OPTIONS" "$@")"
+    URL=$1   
+    FILE_URL=$(curl "$URL" | parse 'download_btn\.jpg' 'href="\([^"]*\)"') || 
+        { error "file not found"; return 1; }
+    test "$CHECK_LINK" && return 255
+    echo "$FILE_URL"
+}
