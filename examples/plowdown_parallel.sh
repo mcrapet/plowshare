ee3ca705fa23b5dbeb04e25c54b26b01e4c9803f
Arnau Sanchez <pyarnau@gmail.com>
2010-01-21 22:34:16 +0000

    add plowdown_parallel
    git-svn-id: https://plowshare.googlecode.com/svn/trunk@698 51fab9ec-ea3e-11dd-80ed-bda8ba2650cd

---
+++ examples/plowdown_parallel.sh
@@ -0,0 +1,49 @@
+#/bin/bash
+#
+# Launch parallel plowdown processes for different websites
+#
+set -e
+ 
+debug() { echo "$@" >&2; }
+
+groupby() {
+  local PREDICATE=$1
+  local RETURN_FUNC=$2
+  local LAST=
+  local FIRST=1
+  while read LINE; do
+    local VALUE=$(echo $LINE | eval $PREDICATE)
+    local RETURN=$(echo $LINE | eval $RETURN_FUNC)
+    if test "$FIRST" = "1"; then
+      echo -n "$VALUE: $RETURN"
+      FIRST=0      
+    elif test "$LAST" = "$VALUE"; then
+      echo -n " $RETURN"
+    else
+      echo
+      echo -n "$VALUE: $RETURN"
+    fi
+    LAST=$VALUE
+  done
+  echo
+}
+
+INFILE=$1
+PIDS=()
+while read MODULE URLS; do
+  debug "Run: plowdown $URLS"
+  plowdown $URLS &
+  PID=$!
+  PIDS[$PID]=$PID
+done < <(cat $INFILE | while read URL; do
+           MODULE=$(plowdown --get-module $URL)
+            echo "$MODULE $URL"
+         done | sort -k1 | groupby "cut -d' ' -f1" "cut -d' ' -f2")
+
+while test ${#PIDS[*]} -ne 0; do
+  #debug "Waiting for pids: ${PIDS[*]}"
+  for PID in ${PIDS[*]}; do
+    kill -0 $PID 2>/dev/null || { wait $PID && unset PIDS[$PID]; }
+  done
+  sleep 1
+done 
