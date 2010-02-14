193e28ae62898fc98443b6e6fe85be8f414c52f3
Arnau Sanchez <pyarnau@gmail.com>
2010-02-14 12:55:47 +0000

    Abstract mark_file. Mark also when no module is found for a link
    git-svn-id: https://plowshare.googlecode.com/svn/trunk@792 51fab9ec-ea3e-11dd-80ed-bda8ba2650cd

--- src/download.sh
+++ src/download.sh
@@ -113,6 +113,16 @@ usage() {
     debug_options_for_modules "$MODULES" "DOWNLOAD"
 }
 
+# If MARK_DOWN enabled and it's a file, comment out URL (with optional TEXT) 
+mark_queue() {
+    local TYPE=$1; local MARK_DOWN=$2; FILE=$3; local URL=$4; local TEXT=$5
+    test "$TYPE" = "file" -a "$MARK_DOWN" || return
+    sed -i -e "s|^[[:space:]]*\($URL\)[[:space:]]*$|#$TEXT \1|" "$FILE" &&
+        debug "link marked in file: $FILE (#$TEXT)" ||
+        error "failed marking link in file: $FILE (#$TEXT)"
+}
+
+
 # download MODULE URL FUNCTION_OPTIONS
 download() {
     local MODULE=$1
@@ -143,11 +153,7 @@ download() {
           break
         elif test $DRETVAL -eq 254; then
           debug "Warning: file link is not alive"
-          if test "$TYPE" = "file" -a "$MARK_DOWN"; then
-              sed -i -e "s|^[[:space:]]*\($URL\)[[:space:]]*$|#NOTFOUND \1|" "$ITEM" &&
-                  debug "link marked as non-downloadable in file: $ITEM" ||
-                  error "failed marking link as non-downloadable in file $ITEM"
-          fi
+          mark_queue "$TYPE" "$MARK_DOWN" "$ITEM" "$URL" "NOTFOUND"
           # Don't set RETVAL, a dead link is not considerer an error¡
           break
         fi
@@ -175,7 +181,8 @@ download() {
             continue_downloads "$MODULE" && CURL=($CURL "-C -")
             test "$LIMIT_RATE" && CURL=($CURL "--limit-rate $LIMIT_RATE")
             test "$COOKIES" && CURL=($CURL -b $COOKIES)
-            CODE=$(${CURL[@]} -w "%{http_code}" -y60 -f --globoff -o "$FILENAME" "$FILE_URL") || DRETVAL=$?
+            CODE=$(${CURL[@]} -w "%{http_code}" -y60 -f --globoff \
+                              -o "$FILENAME" "$FILE_URL") || DRETVAL=$?
             test "$COOKIES" && rm $COOKIES
             if [ $DRETVAL -eq 22 -o $DRETVAL -eq 18 -o $DRETVAL -eq 28 ]; then
                 local WAIT=60
@@ -194,12 +201,8 @@ download() {
             fi
         fi
 
-        echo "$FILENAME"
-        if test "$TYPE" = "file" -a "$MARK_DOWN"; then
-            sed -i -e "s|^[[:space:]]*\($URL\)[[:space:]]*$|#\1|" "$ITEM" &&
-                debug "link marked as downloaded in file: $ITEM" ||
-                error "failed marking link as downloaded in file $ITEM"
-        fi
+        echo "$FILENAME"        
+        mark_queue "$TYPE" "$MARK_DOWN" "$ITEM" "$URL" ""
         break
     done
 }
@@ -222,10 +225,15 @@ for ITEM in "$@"; do
     for INFO in $(process_item "$ITEM"); do
         IFS="|" read TYPE URL <<< "$INFO"
         MODULE=$(get_module "$URL" "$MODULES")
-        test -z "$MODULE" &&
-            { debug "no module for URL: $URL"; RETVAL=$DERROR; continue; }
-        test "$GET_MODULE" &&
-            { echo "$MODULE"; continue; }
+        if test -z "$MODULE"; then
+            debug "no module for URL: $URL"
+            RETVAL=$DERROR
+            mark_queue "$TYPE" "$MARK_DOWN" "$ITEM" "$URL" "NOMODULE"
+            continue
+        elif test "$GET_MODULE"; then
+            echo "$MODULE"
+            continue
+        fi
         download "$MODULE" "$URL" "$LINK_ONLY" "$LIMIT_RATE" "$TYPE" \
             "$MARK_DOWN" "$OUTPUT_DIR" "$CHECK_LINK" "$TIMEOUT" \
             "$MAXRETRIES" "${UNUSED_OPTIONS[@]}"
