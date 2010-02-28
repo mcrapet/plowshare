2db928af337c22337c44168ac3fefbc9b0cdf56c
Matthieu Crapet <mcrapet@gmail.com>
2010-02-28 11:14:31 +0000

    test_setup.sh: add missing humyo module
    git-svn-id: https://plowshare.googlecode.com/svn/trunk@834 51fab9ec-ea3e-11dd-80ed-bda8ba2650cd

--- test/test_setup.sh
+++ test/test_setup.sh
@@ -54,6 +54,7 @@ share/plowshare/modules/depositfiles.sh
 share/plowshare/modules/divshare.sh
 share/plowshare/modules/dl_free_fr.sh
 share/plowshare/modules/hotfile.sh
+share/plowshare/modules/humyo.sh
 share/plowshare/modules/letitbit.sh
 share/plowshare/modules/loadfiles.sh
 share/plowshare/modules/mediafire.sh
