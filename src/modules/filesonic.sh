8d1f5ff763860c5b1251ed226fb019b67f7888d1
StalkR <stalkr@stalkr.net>
2011-04-12 17:03:22 +0000

    filesonic: rework, add upload with account, add delete
    git-svn-id: https://plowshare.googlecode.com/svn/trunk@1434 51fab9ec-ea3e-11dd-80ed-bda8ba2650cd

--- src/modules/filesonic.sh
+++ src/modules/filesonic.sh
@@ -23,10 +23,38 @@ MODULE_FILESONIC_DOWNLOAD_OPTIONS="
 AUTH,a:,auth:,USER:PASSWORD,Use a free-membership or premium account"
 MODULE_FILESONIC_DOWNLOAD_CONTINUE=no
 MODULE_FILESONIC_UPLOAD_OPTIONS="
-AUTH,a:,auth:,USER:PASSWORD,Use an account"
-#MODULE_FILESONIC_DELETE_OPTIONS="
-#AUTH,a:,auth:,USER:PASSWORD,Login to free or premium account (required)"
+AUTH,a:,auth:,USER:PASSWORD,Use a free-membership or premium account"
+MODULE_FILESONIC_DELETE_OPTIONS="
+AUTH,a:,auth:,USER:PASSWORD,Use a free-membership or premium account"
 MODULE_FILESONIC_LIST_OPTIONS=""
+
+
+# Proceed with login (free-membership or premium)
+filesonic_login() {
+    AUTH=$1
+    COOKIES=$2
+    BASEURL=$3
+
+    LOGIN_DATA='email=$USER&password=$PASSWORD'
+    LOGIN=$(post_login "$AUTH" "$COOKIES" "$LOGIN_DATA" \
+        "$BASEURL/user/login" "-H X-Requested-With:XMLHttpRequest -H Accept:application/json")
+
+    if ! test "$LOGIN"; then
+        log_debug "Login error"
+        return 1
+    fi
+
+    STATUS=$(echo "$LOGIN" | parse_quiet '"status":"[^"]*"' '"status":"\([^"]*\)"')
+    if [ "$STATUS" != "success" ]; then
+        log_debug "Login failed: $STATUS"
+        return 1
+    fi
+
+    ROLE=$(getcookie "role" < "$COOKIES")
+    log_notice "Successfully logged in as $ROLE member"
+
+    return 0
+}
 
 # Output an filesonic.com file download URL (anonymous)
 # $1: filesonic url string
@@ -36,7 +64,7 @@ filesonic_download() {
 
     URL=$1
     local ID=$(echo "$URL" | parse_quiet '\/file\/' 'file\/\([^/]*\)')
-    if [ -z "$ID" ]; then
+    if ! test "$ID"; then
         log_error "Cannot parse URL to extract file id (mandatory)"
         return 253
     fi
@@ -44,7 +72,7 @@ filesonic_download() {
     # update URL if there is a specific .ccTLD location from there
     BASEURL=$(basename_url "$URL")
     LOCATION=$(curl -I "$BASEURL" | grep_http_header_location)
-    if [ ! -z "$LOCATION" ]; then
+    if test "$LOCATION"; then
         BASEURL=$(basename_url "$LOCATION")
     fi
     URL="$BASEURL/file/$ID"
@@ -52,34 +80,46 @@ filesonic_download() {
     COOKIES=$(create_tempfile)
 
     # obtain mainpage first (unauthenticated) to get filename
-    MAINPAGE=$(curl -c "$COOKIES" "$URL") || return 1
+    MAINPAGE=$(curl -c "$COOKIES" "$URL") || {
+        rm -f "$COOKIES"
+        return 1
+    }
+
     # do not obtain filename from "<span>Filename:" because it is shortened
     # with "..." if too long; instead, take it from title
     FILENAME=$(echo "$MAINPAGE" | parse_quiet "<title>" ">Download \(.*\) for free")
 
-    # Premium user
-    if [ -n "$AUTH" ]; then
-        LOGIN_DATA='email=$USER&password=$PASSWORD'
-        post_login "$AUTH" "$COOKIES" "$LOGIN_DATA" "$BASEURL/premium?login=1" >/dev/null || {
+    # Attempt to authenticate
+    if test "$AUTH"; then
+        filesonic_login "$AUTH" "$COOKIES" "$BASEURL" || {
             rm -f "$COOKIES"
             return 1
         }
+
         FILE_URL=$(curl -I -b "$COOKIES" "$URL" | grep_http_header_location)
+        if ! test "$FILE_URL"; then
+            log_error "No link received (most likely premium account expired)"
+            rm -f "$COOKIES"
+            return 1
+        fi
 
     # Normal user
     else
         PAGE=$(curl -b "$COOKIES" -H "X-Requested-With: XMLHttpRequest" \
-                    --referer "$URL?start=1" --data "" "$URL?start=1") || return 1
+                    --referer "$URL?start=1" --data "" "$URL?start=1")
 
         if match 'File does not exist' "$PAGE"; then
             log_debug "File not found"
             rm -f "$COOKIES"
             return 254
         fi
-        test "$CHECK_LINK" && return 255
+        if test "$CHECK_LINK"; then
+            rm -f "$COOKIES"
+            return 255
+        fi
 
         # Cases: download link, <400MB, captcha, wait
-        # captcha/wait can redirect to any of these three cases
+        # captcha/wait can redirect to any of the other cases
         FOLLOWS=0
         while [ $FOLLOWS -lt 5 ]; do
             (( FOLLOWS++ ))
@@ -92,6 +132,7 @@ filesonic_download() {
             # free users can download files < 400MB
             elif match 'download is larger than 400Mb.' "$PAGE"; then
                 log_error "You're trying to download file larger than 400MB (only premium users can)."
+                rm -f "$COOKIES"
                 return 255
 
             # captcha
@@ -99,14 +140,14 @@ filesonic_download() {
                 local PUBKEY='6LdNWbsSAAAAAIMksu-X7f5VgYy8bZiiJzlP83Rl'
                 IMAGE_FILENAME=$(recaptcha_load_image $PUBKEY)
 
-                if [ -z "$IMAGE_FILENAME" ]; then
+                if ! test "$IMAGE_FILENAME"; then
                     log_error "reCaptcha error"
                     rm -f "$COOKIES"
                     return 1
                 fi
 
                 TRY=1
-                while retry_limit_not_reached || return 3; do
+                while retry_limit_not_reached || { rm -f "$COOKIES"; return 3; }; do
                     log_debug "reCaptcha manual entering (loop $TRY)"
                     (( TRY++ ))
 
@@ -114,7 +155,7 @@ filesonic_download() {
 
                     rm -f $IMAGE_FILENAME
 
-                    [ -n "$WORD" ] && break
+                    test "$WORD" && break
 
                     log_debug "empty, request another image"
                     IMAGE_FILENAME=$(recaptcha_reload_image $PUBKEY "$IMAGE_FILENAME")
@@ -122,26 +163,31 @@ filesonic_download() {
 
                 CHALLENGE=$(recaptcha_get_challenge_from_image "$IMAGE_FILENAME")
 
-                PAGE=$(curl -b "$COOKIES" -H "X-Requested-With: XMLHttpRequest" --referer "$URL" --data \
-                  "recaptcha_challenge_field=$CHALLENGE&recaptcha_response_field=$WORD" "$URL?start=1") || return 1
+                DATA="recaptcha_challenge_field=$CHALLENGE&recaptcha_response_field=$WORD"
+                PAGE=$(curl -b "$COOKIES" -H "X-Requested-With: XMLHttpRequest" \
+                            --referer "$URL" --data "$DATA" "$URL?start=1")
 
                 match 'Please Enter Captcha' "$PAGE" && log_error "wrong captcha"
 
             # wait
             elif match 'countDownDelay' "$PAGE"; then
-                SLEEP=$(echo "$PAGE" | parse_quiet 'var countDownDelay = ' 'countDownDelay = \([0-9]*\);') || return 1
-                wait $SLEEP seconds || return 2
+                SLEEP=$(echo "$PAGE" | parse_quiet 'var countDownDelay = ' 'countDownDelay = \([0-9]*\);')
+                wait $SLEEP seconds || {
+                    rm -f "$COOKIES"
+                    return 2
+                }
 
                 # for wait time > 5min. these values may not be present
                 # it just means we need to try again so the following code is fine
-                TM=$(echo "$PAGE" | parse_quiet "name='tm' value='" "name='tm' value='\([0-9]*\)'")
-                TM_HASH=$(echo "$PAGE" | parse_quiet "name='tm_hash' value='" "name='tm_hash' value='\([a-f0-9]*\)'")
+                TM=$(echo "$PAGE" | parse_attr "name='tm'" "value")
+                TM_HASH=$(echo "$PAGE" | parse_attr "name='tm_hash'" "value")
 
                 PAGE=$(curl -b "$COOKIES" -H "X-Requested-With: XMLHttpRequest" \
-                            --referer "$URL" --data "tm=$TM&tm_hash=$TM_HASH" "$URL?start=1") || return 1
+                            --referer "$URL" --data "tm=$TM&tm_hash=$TM_HASH" "$URL?start=1")
 
             else
                 log_error "No match. Site update?"
+                rm -f "$COOKIES"
                 return 1
 
             fi
@@ -171,14 +217,14 @@ filesonic_list() {
 
     PAGE=$(curl "$URL" | grep '<a href="http://www.filesonic.com/file/')
 
-    if test -z "$PAGE"; then
+    if ! test "$PAGE"; then
         log_error "Wrong folder link (no download link detected)"
         return 1
     fi
 
     # First pass: print file names (debug)
     echo "$PAGE" | while read LINE; do
-        FILENAME=$(echo "$LINE" | parse 'href' '>\([^<]*\)<\/a>')
+        FILENAME=$(echo "$LINE" | parse_quiet 'href' '>\([^<]*\)<\/a>')
         log_debug "$FILENAME"
     done
 
@@ -194,6 +240,7 @@ filesonic_list() {
 # Upload a file to filesonic
 # $1: file name to upload
 # $2: upload as file name (optional, defaults to $1)
+# $3: member only, upload in folder id (optional, defaults to 0 for root)
 # stdout: download link on filesonic
 filesonic_upload() {
     set -e
@@ -201,28 +248,33 @@ filesonic_upload() {
 
     local FILE=$1
     local DESTFILE=${2:-$FILE}
+    local FOLDERID=${3:-0}
     local URL="http://www.filesonic.com"
 
     # update URL if there is a specific .ccTLD location from there
     LOCATION=$(curl -I "$URL" | grep_http_header_location)
-    if [ ! -z "$LOCATION" ]; then
+    if test "$LOCATION"; then
         URL=$(basename_url "$LOCATION")
     fi
 
     COOKIES=$(create_tempfile)
+    FOLDER=""
 
-    # proceed to auth if credentials supplied
+    # Attempt to authenticate
     if test "$AUTH"; then
-        log_error "auth not yet implemented"
-        return 1
+        filesonic_login "$AUTH" "$COOKIES" "$URL" || {
+            rm -f "$COOKIES"
+            return 1
+        }
+        FOLDER="-F folderId=$FOLDERID"
     fi
 
     # get main page to pick up an upload server
-    PAGE=$(curl -c "$COOKIES" "$URL") || return 1
+    PAGE=$(curl -b "$COOKIES" "$URL") || return 1
     SERVER=$(echo "$PAGE" | parse_quiet 'uploadServerHostname' "\s*=\s'*\([^']*\)';")
 
-    if [ -z "$SERVER" ]; then
-        log_error "Can't parse upload server, site updated?"
+    if ! test "$SERVER"; then
+        log_error "Can't find an upload server, site updated?"
         return 1
     fi
 
@@ -231,22 +283,21 @@ filesonic_upload() {
     ID=$(echo 'var uid = "upload_"+new Date().getTime()+"_'$PHPSESSID'_"+Math.floor(Math.random()*90000); print(uid);' | javascript)
 
     # send file and get Location to completed URL
-    # Note: we don't want curl to send "Expect: 100-continue" header
-    STATUS=$(curl -D - -b "$COOKIES" -e "$URL" -H "Expect:" \
-        -F "files[]=@$FILE;filename=$(basename_file "$DESTFILE")" \
+    # Note: explicitely remove "Expect: 100-continue" header that curl wants to send
+    STATUS=$(curl -D - -b "$COOKIES" --referer "$URL" -H "Expect:" \
+        -F "files[]=@$FILE;filename=$(basename_file "$DESTFILE")" $FOLDER \
         "http://$SERVER/?callbackUrl=$URL/upload-completed/:uploadProgressId&X-Progress-ID=$ID")
 
     if ! test "$STATUS"; then
-        log_error "Uploading error."
+        log_error "Upload error"
+        rm -f "$COOKIES"
+        return 1
+    elif match 'An error occurred' "$STATUS"; then
+        log_error "Upload failed: server error"
         rm -f "$COOKIES"
         return 1
     fi
     COMPLETED=$(echo "$STATUS" | grep_http_header_location)
-
-    # hit keep-alive - not really required, but let's play like a browser
-    TIME=$(echo 'print(new Date().getTime());' | javascript)
-    KEEPALIVE=$(curl -b "$COOKIES" -H "X-Requested-With: XMLHttpRequest" \
-        -e "$URL" "$URL/keep-alive.php?_=$TIME")
 
     # get information
     INFOS=$(curl -b "$COOKIES" -e "$URL" "$COMPLETED")
@@ -268,7 +319,7 @@ filesonic_upload() {
     LINKID=$(echo "$INFOS" | parse_quiet '"linkId":"[^"]*"' '"linkId":"\([^"]*\)"')
     BROWSE=$(curl -b "$COOKIES" -H "X-Requested-With: XMLHttpRequest" \
         -e "$URL" "$URL/filesystem/generate-link/$LINKID")
-    LINK=$(echo "$BROWSE" | parse_attr ' id="URL_' 'value')
+    LINK=$(echo "$BROWSE" | parse_attr 'id="URL_' 'value')
 
     rm -f "$COOKIES"
 
@@ -276,6 +327,69 @@ filesonic_upload() {
         log_error "Can't parse download link, site updated?"
         return 1
     fi
+
     echo "$LINK"
+
+    return 0
+}
+
+# Delete a file on filesonic (requires an account)
+# $1: download link (must be a file that account uploaded)
+# stdout: download link on filesonic
+filesonic_delete() {
+    set -e
+    eval "$(process_options filesonic "$MODULE_FILESONIC_DELETE_OPTIONS" "$@")"
+
+    if ! test "$AUTH"; then
+        log_error "Anonymous users cannot delete links."
+        return 1
+    fi
+
+    URL=$1
+    local ID=$(echo "$URL" | parse_quiet '\/file\/' 'file\/\([^/]*\)')
+    if ! test "$ID"; then
+        log_error "Cannot parse URL to extract file id (mandatory)"
+        return 253
+    fi
+
+    # update URL if there is a specific .ccTLD location from there
+    URL=$(basename_url "$URL")
+    LOCATION=$(curl -I "$URL" | grep_http_header_location)
+    if test "$LOCATION"; then
+        URL=$(basename_url "$LOCATION")
+    fi
+
+    COOKIES=$(create_tempfile)
+
+    # Attempt to authenticate
+    filesonic_login "$AUTH" "$COOKIES" "$URL" || {
+        rm -f "$COOKIES"
+        return 1
+    }
+
+    # Delete file, identifier is "F"+ID
+    DELETE=$(curl -b "$COOKIES" -H "Accept: application/json" \
+                -H "X-Requested-With: XMLHttpRequest" \
+                --referer "$URL/filesystem/browse" \
+                --data "files%5B%5D=F$ID" \
+                "$URL/filesystem/delete")
+
+    rm -f "$COOKIES"
+
+    if ! test "$DELETE"; then
+        log_debug "Delete error"
+        return 1
+    elif match 'Item not found' "$DELETE"; then
+        log_error "Not found or already deleted"
+        return 254
+    fi
+
+    STATUS=$(echo "$DELETE" | parse_quiet '"status":"[^"]*"' '"status":"\([^"]*\)"')
+    if [ "$STATUS" != "success" ]; then
+        log_debug "Delete failed: $STATUS"
+        return 1
+    fi
+
+    log_notice "File deleted"
     return 0
 }
