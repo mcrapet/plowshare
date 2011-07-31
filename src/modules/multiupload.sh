6cda426d6908f0e6f3456dabf908741411a78ec3
Matthieu Crapet <mcrapet@gmail.com>
2011-07-31 09:05:46 +0000

    multiupload: add --no-up, --email-from, --email-to options + add -0 curl option
    git-svn-id: https://plowshare.googlecode.com/svn/trunk@1643 51fab9ec-ea3e-11dd-80ed-bda8ba2650cd

--- src/modules/multiupload.sh
+++ src/modules/multiupload.sh
@@ -22,7 +22,10 @@ MODULE_MULTIUPLOAD_REGEXP_URL="http://\(www\.\)\?multiupload\.com/"
 
 MODULE_MULTIUPLOAD_UPLOAD_OPTIONS="
 DESCRIPTION,d:,description:,DESCRIPTION,Set file description
-AUTH,a:,auth:,USER:PASSWORD,User account"
+AUTH,a:,auth:,USER:PASSWORD,User account
+FROMEMAIL,,email-from:,EMAIL,<From> field for notification email
+TOEMAIL,,email-to:,EMAIL,<To> field for notification email
+NO_UPLOADING_COM,,no-up,,Exclude Uploading.com from host list"
 MODULE_MULTIUPLOAD_LIST_OPTIONS=""
 
 # Upload a file to multiupload.com
@@ -41,14 +44,17 @@ multiupload_upload() {
     local BASE_URL="http://www.multiupload.com"
 
     if test "$AUTH"; then
-        local USER="${AUTH%%:*}"
-        local PASSWORD="${AUTH#*:}"
+        local USER PASSWORD LOGIN_RESULT
+
+        USER="${AUTH%%:*}"
+        PASSWORD="${AUTH#*:}"
 
         if [ "$AUTH" = "$PASSWORD" ]; then
             PASSWORD=$(prompt_for_password) || return $ERR_LOGIN_FAILED
         fi
 
-        local LOGIN_RESULT=$(curl -L -c "$COOKIEFILE" -F "username=$USER" -F "password=$PASSWORD" "$BASE_URL/login")
+        LOGIN_RESULT=$(curl -L -c "$COOKIEFILE" -F "username=$USER" \
+                -F "password=$PASSWORD" "$BASE_URL/login") || return
 
         if ! match 'Logged in' "$LOGIN_RESULT"; then
             return $ERR_LOGIN_FAILED
@@ -59,10 +65,10 @@ multiupload_upload() {
 
     local form=$(grep_form_by_id "$PAGE" uploadfrm)
     local form_action=$(echo "$form" | parse_form_action)
-    local form_u=$(echo "$form" |  parse_form_input_by_name 'u')
+    local form_u=$(echo "$form" | parse_form_input_by_name 'u')
     local form_x=$(echo "$form" | parse_form_input_by_name 'X-Progress-ID')
 
-    log_debug "Upload ID: $form_u / $form_x"
+    log_debug "Upload ID: $form_u / ${form_x:-No Progress-ID}"
 
     # keep default settings
     local form_site1=$(echo "$form" | parse_form_input_by_name 'service_5')
@@ -71,13 +77,17 @@ multiupload_upload() {
     local form_site4=$(echo "$form" | parse_form_input_by_name 'service_9')
     local form_site5=$(echo "$form" | parse_form_input_by_name 'service_6')
     local form_site6=$(echo "$form" | parse_form_input_by_name 'service_10')
+    local form_site7=$(echo "$form" | parse_form_input_by_name 'service_15')
+    local form_site8=$(echo "$form" | parse_form_input_by_name 'service_14')
+
+    test "$NO_UPLOADING_COM" && form_site6=''
 
     # Notes:
     # - file0 can go to file9 (included)
     # - fetchfield0 & fetchdesc0 are not used here
     # - there is a special variable "rsaccount" for RS (can be "C" or "P")
     # - hosters: RS, MU, DF, HF, ZS, UP, FC, FS
-    PAGE=$(curl_with_log -b "$COOKIEFILE" \
+    PAGE=$(curl_with_log -0 -b "$COOKIEFILE" \
         -F "file0=@$FILE;filename=$(basename_file "$DESTFILE")" \
         -F "description_0=$DESCRIPTION" \
         -F "X-Progress-ID=$form_x" \
@@ -88,9 +98,9 @@ multiupload_upload() {
         -F "service_9=$form_site4"  -F "username_9="  -F "password_9="  -F "remember_9="  \
         -F "service_6=$form_site5"  -F "username_6="  -F "password_6="  -F "remember_6="  \
         -F "service_10=$form_site6" -F "username_10=" -F "password_10=" -F "remember_10=" \
-        -F "service_15=$form_site6" -F "username_15=" -F "password_15=" -F "remember_15=" \
-        -F "service_14=$form_site6" -F "username_14=" -F "password_14=" -F "remember_14=" \
-        -F "fromemail=plowshare@gmx.com" -F "toemail=" $form_action) || return
+        -F "service_15=$form_site7" -F "username_15=" -F "password_15=" -F "remember_15=" \
+        -F "service_14=$form_site8" -F "username_14=" -F "password_14=" -F "remember_14=" \
+        -F "fromemail=$FROMEMAIL" -F "toemail=$TOEMAIL" $form_action) || return
 
     DLID=$(echo "$PAGE" | parse_quiet 'downloadid' 'downloadid":"\([^"]*\)')
     log_debug "Download ID: $DLID"
