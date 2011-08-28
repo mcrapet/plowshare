9b4cfb6fb3e27c9f49047b488798a3419753736d
Matthieu Crapet <mcrapet@gmail.com>
2011-08-28 13:45:27 +0200

    badongo (download): fix, site update (javascript code is 'escaped')

--- src/modules/badongo.sh
+++ src/modules/badongo.sh
@@ -34,7 +34,8 @@ badongo_download() {
     local BASEURL="http://www.badongo.com"
     local APIURL="${BASEURL}/ajax/prototype/ajax_api_filetemplate.php"
 
-    PAGE=$(curl "$URL")
+    PAGE=$(curl "$URL") || return
+
     match '"recycleMessage">' "$PAGE" &&
         { log_debug "file in recycle bin"; return $ERR_LINK_DEAD; }
     match '"fileError">' "$PAGE" &&
@@ -43,7 +44,7 @@ badongo_download() {
     detect_javascript >/dev/null || return
     PERL_PRG=$(detect_perl) || return
 
-    TRY=1
+    local TRY=1
 
     while retry_limit_not_reached || return; do
         log_debug "Downloading captcha page (loop $TRY)"
@@ -52,20 +53,32 @@ badongo_download() {
             -F "rs=refreshImage" \
             -F "rst=" \
             -F "rsrnd=$MTIME" \
-            "$URL" | break_html_lines)
+            "$URL" | break_html_lines_alt)
 
         ACTION=$(echo "$JSCODE" | parse "form" 'action=\\"\([^\\]*\)\\"') ||
             { log_debug "file not found"; return $ERR_LINK_DEAD; }
 
         test "$CHECK_LINK" && return 0
 
-        # 200x60 jpeg file
-        CAP_IMAGE=$(echo "$JSCODE" | parse '<img' 'src=\\"\([^\\]*\)\\"')
         MTIME="$(date +%s)000"
-        CAPTCHA=$(curl $BASEURL$CAP_IMAGE | $PERL_PRG $LIBDIR/strip_threshold.pl 125 | \
-            convert - +matte -colorspace gray -level 45%,45% gif:- | \
-            show_image_and_tee | ocr upper | sed "s/[^a-zA-Z]//g") ||
-                { log_error "unexpected result from OCR"; continue; }
+
+        # 200x60 jpeg file
+        CAPTCHA_URL=$(echo "$JSCODE" | parse '<img' 'src=\\"\([^\\]*\)\\"')
+
+        CAPTCHA_IMG=$(create_tempfile) || return
+        curl "$BASEURL$CAPTCHA_URL" | $PERL_PRG $LIBDIR/strip_threshold.pl 125 | \
+                convert - +matte -colorspace gray -level 45%,45% gif:$CAPTCHA_IMG || { \
+            rm -f "$CAPTCHA_IMG"
+            return $ERR_CAPTCHA;
+        }
+
+        #CAPTCHA=$(captcha_process "$CAPTCHA_IMG" auto) || return
+        CAPTCHA=$(cat "$CAPTCHA_IMG" | ocr upper | sed "s/[^a-zA-Z]//g") || { \
+            log_error "error running OCR";
+            rm -f "$CAPTCHA_IMG"
+            return $ERR_CAPTCHA;
+        }
+        rm -f "$CAPTCHA_IMG"
 
         test "${#CAPTCHA}" -gt 4 && CAPTCHA="${CAPTCHA:0:4}"
         log_debug "Decoded captcha: $CAPTCHA"
@@ -77,24 +90,31 @@ badongo_download() {
 
         CAP_ID=$(echo "$JSCODE" | parse_form_input_by_name 'cap_id')
         CAP_SECRET=$(echo "$JSCODE" | parse_form_input_by_name 'cap_secret')
+
         WAIT_PAGE=$(curl -c $COOKIEFILE \
             --data "user_code=${CAPTCHA}&cap_id=${CAP_ID}&cap_secret=$CAP_SECRET" \
             "$ACTION")
+
         match 'id="link_container"' "$WAIT_PAGE" && break
         log_debug "Wrong captcha"
     done
 
     log_debug "Correct captcha!"
 
-    # Look for doDownload function
-    LINK_PART2=$(echo "$WAIT_PAGE" | grep 'window.location.href' | last_line | \
-            parse 'location\.href' '+ "\([^"]*\)') ||
-        { log_error "error parsing link part2, site updated?"; return 1; }
+    JSCODE=$(unescape_javascript "$WAIT_PAGE" 6)
 
-    # Look for window.ck_* variable (timer)
-    WAIT_TIME=$(echo "$WAIT_PAGE" | parse_last 'window\.ck_' '[[:space:]]=[[:space:]]\([[:digit:]]\+\)')
-    GLF_Z=$(echo "$WAIT_PAGE" | parse_last 'window\.getFileLinkInitOpt' "z = '\([^']*\)")
-    GLF_H=$(echo "$WAIT_PAGE" | parse_last 'window\.getFileLinkInitOpt' "'h':'\([^']*\)")
+    # Look for doDownload function (now obfuscated)
+    LINK_PART2=$(echo "$JSCODE" | grep 'window.location.href' | last_line | \
+            parse 'location\.href' '+ "\([^"]*\)') ||
+        { log_error "error parsing link part2, site updated?"; return $ERR_FATAL; }
+
+    JSCODE=$(unescape_javascript "$WAIT_PAGE" 2)
+
+    # Look for window.[0-9A-F]{25} variable (timer)
+    #WAIT_TIME=$(echo "$WAIT_PAGE" | parse_last 'window\.ck_' '[[:space:]]=[[:space:]]\([[:digit:]]\+\)')
+    WAIT_TIME=60
+    GLF_Z=$(echo "$JSCODE" | parse_last 'window\.getFileLinkInitOpt' "z = '\([^']*\)")
+    GLF_H=$(echo "$JSCODE" | parse_last 'window\.getFileLinkInitOpt' "'h':'\([^']*\)")
     FILEID="${ACTION##*/}"
     FILETYPE='file'
 
@@ -103,12 +123,13 @@ badongo_download() {
             --data "id=${FILEID}&type=${FILETYPE}&ext=&f=download%3Ainit&z=${GLF_Z}&h=${GLF_H}" \
             --referer "$ACTION" "$APIURL") ||
         { log_error "error json (#1), site updated?"; return 1; }
+    JSCODE=$(unescape_javascript "$JSON" 1)
 
     # Parse received window['getFileLinkInitOpt'] object
     # Get new values of GLF_Z and GLF_H
-    GLF_Z=$(echo "$JSON" | parse "'z'" "[[:space:]]'\([^']*\)");
-    GLF_H=$(echo "$JSON" | parse "'h'" "[[:space:]]'\([^']*\)");
-    GLF_T=$(echo "$JSON" | parse "'t'" "[[:space:]]'\([^']*\)");
+    GLF_Z=$(echo "$JSCODE" | parse "'z'" "[[:space:]]'\([^']*\)");
+    GLF_H=$(echo "$JSCODE" | parse "'h'" "[[:space:]]'\([^']*\)");
+    GLF_T=$(echo "$JSCODE" | parse "'t'" "[[:space:]]'\([^']*\)");
 
     # Usual wait time is 60 seconds
     wait $((WAIT_TIME)) seconds || return
@@ -119,31 +140,43 @@ badongo_download() {
             --referer "$ACTION" \
             "$APIURL") ||
         { log_error "error json (#2), site updated?"; return 1; }
+    JSCODE=$(unescape_javascript "$JSON" 1)
 
     # Parse again received window['getFileLinkInitOpt'] object
     # Get new values of GLF_Z, GLF_H and GLF_T (and escape '!' character)
-    GLF_Z=$(echo "$JSON" | parse "'z'" "[[:space:]]'\([^']*\)" | replace '!' '%21');
-    GLF_H=$(echo "$JSON" | parse "'h'" "[[:space:]]'\([^']*\)" | replace '!' '%21');
-    GLF_T=$(echo "$JSON" | parse "'t'" "[[:space:]]'\([^']*\)" | replace '!' '%21');
+    GLF_Z=$(echo "$JSCODE" | parse "'z'" "[[:space:]]'\([^']*\)" | replace '!' '%21');
+    GLF_H=$(echo "$JSCODE" | parse "'h'" "[[:space:]]'\([^']*\)" | replace '!' '%21');
+    GLF_T=$(echo "$JSCODE" | parse "'t'" "[[:space:]]'\([^']*\)" | replace '!' '%21');
 
     # HTTP GET request
     JSCODE=$(curl -G -b "_gflCur=0" -b $COOKIEFILE \
         --data "rs=getFileLink&rst=&rsrnd=${MTIME}&rsargs[]=0&rsargs[]=yellow&rsargs[]=${GLF_Z}&rsargs[]=${GLF_H}&rsargs[]=${GLF_T}&rsargs[]=${FILETYPE}&rsargs[]=${FILEID}&rsargs[]=" \
         --referer "$ACTION" "$ACTION" | break_html_lines)
 
-    # Example: <a href=\"#\" onclick=\"return doDownload(\'http://www.badongo.com/fd/0101052591990549/CCI97956a6891950969/0\');\">
-    # doDownload name is now obfuscated (example: aeacfeededbb)
-    LINK_PART1=$(echo "$JSCODE" | parse_last 'return' "\\\\'\(http[^\\]*\)") ||
-            { log_error "can't parse base url"; return 1; }
+    LINK_PART1=$(echo "$JSCODE" | parse_last 'javascript' "\\\\'\(http[^\\]*\)") ||
+            { log_error "can't parse base url"; return $ERR_FATAL; }
+
+    # Example: http://www.badongo.com/fd/0294088241036178/CCI9368696599696972/0/I!18b47cc7!9ccbab6f6695639867936a6d9969?zenc=
     FILE_URL="${LINK_PART1}${LINK_PART2}?zenc="
 
-    LAST_PAGE=$(curl -b "_gflCur=0" -b $COOKIEFILE --referer "$ACTION" $FILE_URL)
+    LAST_PAGE=$(curl -b "_gflCur=0" -b $COOKIEFILE --referer "$ACTION" $FILE_URL) || return
+    JSCODE=$(unescape_javascript "$LAST_PAGE" 5)
 
     # Look for new location.href
-    LINK_FINAL=$(echo "$LAST_PAGE" | parse_last 'location\.href' "= '\([^']*\)") ||
-        { log_error "error parsing link part2, site updated?"; return 1; }
+    LINK_FINAL=$(echo "$JSCODE" | parse_last 'location\.href' "= '\([^']*\)") ||
+        { log_error "error parsing final link, site updated?"; return 1; }
     FILE_URL=$(curl -i -b $COOKIEFILE --referer "$FILE_URL" "${BASEURL}${LINK_FINAL}" | grep_http_header_location)
 
     test "$FILE_URL" || { log_error "location not found"; return 1; }
     echo "$FILE_URL"
+}
+
+# Decode 'escaped' javascript code
+# $1: HTML data
+# $2: nth element to grep (index start at 1)
+unescape_javascript() {
+    local CODE=$(echo "$1" | grep '^eval((' | nth_line $2)
+    (echo 'decodeURIComponent = function(x) { print(x); return x; }' && \
+        echo 'var Event = { observe: function(a,b,c) {} }; var window="";' && \
+        echo "$CODE") | javascript
 }
