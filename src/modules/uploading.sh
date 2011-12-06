f1f7cea4fb9abc6d78c885b72449cd0c8585c474
Matthieu Crapet <mcrapet@gmail.com>
2011-12-06 21:21:49 +0100

    uploading (download): rework (cosmetics, coding rules)
    - get rid of "while retry_limit_not_reached || return; do" loop
    - use regular 'return $LINK_TEMP_UNAVAILABLE' mecanism
    - declare local variables

--- src/modules/uploading.sh
+++ src/modules/uploading.sh
@@ -29,59 +29,57 @@ MODULE_UPLOADING_DOWNLOAD_FINAL_LINK_NEEDS_COOKIE=yes
 # $2: uploading.com url
 # stdout: real file download link
 uploading_download() {
+    eval "$(process_options uploading "$MODULE_UPLOADING_DOWNLOAD_OPTIONS" "$@")"
+
     local COOKIEFILE="$1"
     local URL="$2"
-    local BASE_URL="http://uploading.com"
+    local BASE_URL='http://uploading.com'
+    local DATA ERR1 ERR2 WAIT JSURL FILENAME FILE_URL
 
-    while retry_limit_not_reached || return; do
-        # Force language to English
-        DATA=$(curl --cookie-jar "$COOKIEFILE" --cookie "lang=1" "$URL")
-        ERR1="Your IP address.*file"
-        ERR2="Sorry, you have reached your daily download limit."
-        if match "$ERR1\|$ERR2" "$DATA"; then
-            WAITTIME=1
-            wait $WAITTIME minutes || return
-            continue
-        fi
+    # Force language to English
+    DATA=$(curl -c "$COOKIEFILE" -b "lang=1" "$URL") || return
 
-        match "requested file is not found" "$DATA" && return $ERR_LINK_DEAD
+    if match "requested file is not found" "$DATA"; then
+        log_debug "File not found"
+        return $ERR_LINK_DEAD
+    fi
 
-        if match "<h2.*Download Limit.*</h2>" "$DATA"; then
-            test "$CHECK_LINK" && return 0
+    test "$CHECK_LINK" && return 0
 
-            WAIT=$(echo "$DATA" | parse "download only one" "one file per \([[:digit:]]\+\) minute") ||
-                WAIT=5
-            log_debug "Server asked to wait"
-            wait $WAIT minutes || return
-            continue
-        fi
+    ERR1="Your IP address.*file"
+    ERR2="Sorry, you have reached your daily download limit."
+    if match "$ERR1\|$ERR2" "$DATA"; then
+        return $ERR_LINK_TEMP_UNAVAILABLE
 
-        HTML_FORM=$(grep_form_by_id "$DATA" 'downloadform')
-        WAIT_URL=$(echo "$HTML_FORM" | parse_form_action) ||
-            { log_error "can't get wait url"; return 1; }
+    elif match "<h2.*Download Limit.*</h2>" "$DATA"; then
+        log_debug "Server asked to wait"
+        WAIT=$(echo "$DATA" | parse "download only one" "one file per \([[:digit:]]\+\) minute")
+        test -n "$WAIT" && echo $((WAIT*60))
+        return $ERR_LINK_TEMP_UNAVAILABLE
+    fi
 
-        test "$CHECK_LINK" && return 0
+    local FORM_HTML FORM_URL FORM_FID FORM_CODE
+    FORM_HTML=$(grep_form_by_id "$DATA" 'downloadform')
+    FORM_URL=$(echo "$FORM_HTML" | parse_form_action) || return
+    FORM_FID=$(echo "$FORM_HTML" | parse_form_input_by_name 'file_id')
+    FORM_CODE=$(echo "$FORM_HTML" | parse_form_input_by_name 'code')
 
-        FILE_ID=$(echo "$HTML_FORM" | parse_form_input_by_name 'file_id') ||
-            { log_error "can't get file_id form field"; return 1; }
-        CODE=$(echo "$HTML_FORM" | parse_form_input_by_name 'code') ||
-            { log_error "can't get code form field"; return 1; }
-
-        DATA=$(curl --cookie "$COOKIEFILE" --cookie "lang=1" --data "action=second_page&file_id=${FILE_ID}&code=${CODE}" "$WAIT_URL") ||
-            { log_error "can't get wait URL contents"; return 1; }
-        break
-    done
+    DATA=$(curl -b "$COOKIEFILE" -b "lang=1" --data \
+        "action=second_page&file_id=${FORM_FID}&code=$FORM_CODE" "$FORM_URL") || return
 
     WAIT=$(echo "$DATA" | parse_quiet 'start_timer([[:digit:]]\+)' \
-            'start_timer(\([[:digit:]]\+\))')
+        'start_timer(\([[:digit:]]\+\))')
     test -z "$WAIT" && WAIT=$(echo "$DATA" | parse 'var[[:space:]]*timer_count' \
-            'timer_count[[:space:]]*=[[:space:]]*\([[:digit:]]\+\);')
+        'timer_count[[:space:]]*=[[:space:]]*\([[:digit:]]\+\);')
 
-    test "$WAIT" || { log_error "Cannot get wait time"; return 1; }
+    if test -z "$WAIT"; then
+        log_error "Cannot get wait time"
+        return $ERR_FATAL
+    fi
+
     JSURL="$BASE_URL/files/get/?JsHttpRequest=$(date +%s000)-xml"
-
-    FILENAME=$(echo "$DATA" | \
-        parse_quiet '<title>' '<title>Download \(.*\) for free on uploading.com<\/title>')
+    FILENAME=$(echo "$DATA" | parse_quiet '<title>' \
+        '<title>Download \(.*\) for free on uploading.com<\/title>')
 
     # Second attempt (note: filename might be truncated in the page)
     test -z "$FILENAME" &&
@@ -89,14 +87,13 @@ uploading_download() {
 
     wait $WAIT seconds || return
 
-    DATA=$(curl --cookie "$COOKIEFILE" --data "action=get_link&file_id=${FILE_ID}&code=${CODE}&pass=undefined" "$JSURL") ||
-        { log_error "can't get link"; return 1; }
+    DATA=$(curl -b "$COOKIEFILE" --data \
+        "action=get_link&file_id=${FORM_FID}&code=${FORM_CODE}&pass=undefined" "$JSURL") || return
 
     # Example of answer:
     # { "id": "1268521606000", "js": { "answer": { "link": "http:\/\/up3.uploading.com\/get_file\/%3D%3DwARfyFZ3fKB8rJ ... " } }, "text": "" }
-    FILE_URL=$(echo "$DATA" | parse '"answer":' '"link":[[:space:]]*"\([^"]*\)"') ||
-        { log_error "URL not found"; return 1; }
+    FILE_URL=$(echo "$DATA" | parse '"answer":' '"link":[[:space:]]*"\([^"]*\)"') || return
 
-    echo $FILE_URL
-    echo $FILENAME
+    echo "$FILE_URL"
+    echo "$FILENAME"
 }
