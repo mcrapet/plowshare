37c5119203603a275cbe2a24dc71cfe4269ea9fd
Matthieu Crapet <mcrapet@gmail.com>
2011-12-10 16:54:02 +0100

    rapidshare: rework (cosmetics, coding rules)
    - enable download resume for anonymous users (yes, that works!)
    - get rid of "while retry_limit_not_reached || return; do" loop
    - declare local variables

--- src/modules/rapidshare.sh
+++ src/modules/rapidshare.sh
@@ -21,16 +21,16 @@
 MODULE_RAPIDSHARE_REGEXP_URL="https\?://\(www\.\|rs[[:digit:]][0-9a-z]*\.\)\?rapidshare\.com/"
 
 MODULE_RAPIDSHARE_DOWNLOAD_OPTIONS="
-AUTH,a:,auth:,USER:PASSWORD,Use Premium-Zone account"
-MODULE_RAPIDSHARE_DOWNLOAD_RESUME=no
+AUTH,a:,auth:,USER:PASSWORD,User account"
+MODULE_RAPIDSHARE_DOWNLOAD_RESUME=yes
 MODULE_RAPIDSHARE_DOWNLOAD_FINAL_LINK_NEEDS_COOKIE=unused
 
 MODULE_RAPIDSHARE_UPLOAD_OPTIONS="
-AUTH,a:,auth:,USER:PASSWORD,Use an account"
+AUTH,a:,auth:,USER:PASSWORD,User account"
 MODULE_RAPIDSHARE_DELETE_OPTIONS="
-AUTH,a:,auth:,USER:PASSWORD,Use an account"
+AUTH,a:,auth:,USER:PASSWORD,User account"
 
-# Output a rapidshare file download URL (anonymous and premium)
+# Output a rapidshare file download URL
 # $1: cookie file (unused here)
 # $2: rapidshare.com url
 # stdout: real file download link
@@ -38,8 +38,9 @@ rapidshare_download() {
     eval "$(process_options rapidshare "$MODULE_RAPIDSHARE_DOWNLOAD_OPTIONS" "$@")"
 
     local URL="$2"
+    local FILEID FILENAME BASE_URL PARAMS PAGE ERROR WAIT
 
-    # Two URL formats:
+    # Two possible URL format
     # http://rapidshare.com/files/429795114/arc02f.rar
     # http://rapidshare.com/#!download|774tl4|429794114|arc02f.rar|5249
     if match '.*/#!download|' "$URL"; then
@@ -49,75 +50,76 @@ rapidshare_download() {
         FILEID=$(echo "$URL" | cut -d'/' -f5)
         FILENAME=$(echo "$URL" | cut -d'/' -f6)
     fi
-    test "$FILEID" -a "$FILENAME" || \
-        { log_error "Cannot parse fileID/filename from URL: $URL"; return $ERR_FATAL; }
 
-    # Arbitrary wait (local variables)
-    NO_FREE_SLOT_IDLE=125
-    STOP_FLOODING=360
+    if [ -z "$FILEID" -o -z "$FILENAME" ]; then
+        log_error "Cannot parse fileID/filename from URL: $URL"
+        return $ERR_FATAL
+    fi
 
-    while retry_limit_not_reached || return; do
-        BASE_APIURL="https://api.rapidshare.com/cgi-bin/rsapi.cgi?sub=download&fileid=${FILEID}&filename=${FILENAME}"
+    BASE_URL="https://api.rapidshare.com/cgi-bin/rsapi.cgi?sub=download&fileid=${FILEID}&filename=${FILENAME}"
 
-        if test "$AUTH"; then
-            IFS=":" read USER PASSWORD <<< "$AUTH"
-            PARAMS="&login=$USER&password=$PASSWORD"
-        else
-            PARAMS=""
+    if test "$AUTH"; then
+        IFS=":" read USER PASSWORD <<< "$AUTH"
+        if [ -z "$PASSWORD" ]; then
+            PASSWORD=$(prompt_for_password) || return $ERR_LOGIN_FAILED
         fi
+        PARAMS="&login=$USER&password=$PASSWORD"
+    else
+        PARAMS=""
+    fi
 
-        PAGE=$(curl "${BASE_APIURL}${PARAMS}") || return
-        local ERROR=$(echo "$PAGE" | parse_quiet "ERROR:" "ERROR:[[:space:]]*\(.*\)")
+    PAGE=$(curl "${BASE_URL}$PARAMS") || return
+    ERROR=$(echo "$PAGE" | parse_quiet "ERROR:" "ERROR:[[:space:]]*\(.*\)")
 
-        if match "need to wait" "$ERROR"; then
-            WAIT=$(echo "$ERROR" | parse "." "wait \([[:digit:]]\+\) seconds") || return
-
-            test "$CHECK_LINK" && return 0
-            log_notice "Server has asked to wait $WAIT seconds"
-            wait $WAIT seconds || return
-            continue
-
-        elif match "File \(deleted\|not found\|ID invalid\)" "$ERROR"; then
-            return $ERR_LINK_DEAD
-
-        elif match "flooding" "$ERROR"; then
-            log_debug "Server said we are flooding it."
-            echo $STOP_FLOODING
-            return $ERR_LINK_TEMP_UNAVAILABLE
-
-        elif match "slots" "$ERROR"; then
-            log_debug "Server said there is no free slots available"
-            echo $NO_FREE_SLOT_IDLE
-            return $ERR_LINK_TEMP_UNAVAILABLE
-
-        elif test "$ERROR"; then
-            log_error "website error: $ERROR"
-            return $ERR_FATAL
-        fi
-
-        # DL:$hostname,$dlauth,$countdown,$md5hex
-        IFS="," read RSHOST DLAUTH WTIME CRC <<< "${PAGE#DL:}"
-
-        if [ -z "$RSHOST" -o -z "$DLAUTH" -o -z "$WTIME" -o -z "$CRC" ]; then
-            log_error "unexpected page content"
-            return $ERR_FATAL
-        fi
-
+    if match "need to wait" "$ERROR"; then
+        WAIT=$(echo "$ERROR" | parse "." "wait \([[:digit:]]\+\) seconds") || return
         test "$CHECK_LINK" && return 0
+        log_notice "Server has asked to wait $WAIT seconds"
+        echo $((WAIT))
+        return $ERR_LINK_TEMP_UNAVAILABLE
 
-        log_debug "file md5: $CRC"
-        break
-    done
+    elif match "File \(deleted\|not found\|ID invalid\)" "$ERROR"; then
+        return $ERR_LINK_DEAD
+
+    elif match "flooding" "$ERROR"; then
+        log_debug "Server said we are flooding it."
+        echo 360
+        return $ERR_LINK_TEMP_UNAVAILABLE
+
+    elif match "slots" "$ERROR"; then
+        log_debug "Server said there is no free slots available"
+        return $ERR_LINK_TEMP_UNAVAILABLE
+
+    elif matchi 'Login failed' "$ERROR"; then
+        return $ERR_LOGIN_FAILED
+
+    # RapidPro expired. (34fa3175)
+    elif test "$ERROR"; then
+        log_error "website error: $ERROR"
+        return $ERR_FATAL
+    fi
+
+    # DL:$hostname,$dlauth,$countdown,$md5hex
+    IFS="," read RSHOST DLAUTH WTIME CRC <<< "${PAGE#DL:}"
+
+    if [ -z "$RSHOST" -o -z "$DLAUTH" -o -z "$WTIME" -o -z "$CRC" ]; then
+        log_error "unexpected page content"
+        return $ERR_FATAL
+    fi
+
+    test "$CHECK_LINK" && return 0
+
+    log_debug "file md5: $CRC"
 
     wait $((WTIME)) seconds || return
 
     # https is only available for RapidPro customers
-    local BASEURL="://$RSHOST/cgi-bin/rsapi.cgi?sub=download"
+    BASE_URL="://$RSHOST/cgi-bin/rsapi.cgi?sub=download"
 
     if test "$AUTH"; then
-        echo "https$BASEURL&fileid=$FILEID&filename=$FILENAME&dlauth=$DLAUTH&login=$USER&password=$PASSWORD"
+        echo "https$BASE_URL&fileid=$FILEID&filename=$FILENAME&dlauth=$DLAUTH&login=$USER&password=$PASSWORD"
     else
-        echo "http$BASEURL&fileid=$FILEID&filename=$FILENAME&dlauth=$DLAUTH"
+        echo "http$BASE_URL&fileid=$FILEID&filename=$FILENAME&dlauth=$DLAUTH"
     fi
     echo "$FILENAME"
 }
@@ -132,30 +134,31 @@ rapidshare_upload() {
 
     local FILE="$2"
     local DESTFILE="$3"
+    local USER PASSWORD SERVER_NUM UPLOAD_URL INFO ERROR
 
-    if ! test "$AUTH"; then
+    if [ -z "$AUTH" ]; then
         log_error "Anonymous users cannot upload files"
         return $ERR_LINK_NEED_PERMISSIONS
     fi
 
-    local USER="${AUTH%%:*}"
-    local PASSWORD="${AUTH#*:}"
-
+    USER="${AUTH%%:*}"
+    PASSWORD="${AUTH#*:}"
     if [ "$AUTH" = "$PASSWORD" ]; then
         PASSWORD=$(prompt_for_password) || return $ERR_LOGIN_FAILED
     fi
 
-    SERVER_NUM=$(curl "http://api.rapidshare.com/cgi-bin/rsapi.cgi?sub=nextuploadserver") || return
+    SERVER_NUM=$(curl 'http://api.rapidshare.com/cgi-bin/rsapi.cgi?sub=nextuploadserver') || return
     log_debug "upload server is rs$SERVER_NUM"
 
     UPLOAD_URL="https://rs${SERVER_NUM}.rapidshare.com/cgi-bin/rsapi.cgi"
 
     INFO=$(curl_with_log -F "sub=upload" \
-            -F "filecontent=@$FILE;filename=$DESTFILE" \
-            -F "login=$USER" -F "password=$PASSWORD" "$UPLOAD_URL") || return
+        -F "filecontent=@$FILE;filename=$DESTFILE" \
+        -F "login=$USER" -F "password=$PASSWORD" "$UPLOAD_URL") || return
 
     if ! match '^COMPLETE' "$INFO"; then
-        log_error "incomplete upload"
+        ERROR=$(echo "$INFO" | parse_quiet "ERROR:" "ERROR:[[:space:]]*\(.*\)")
+        log_error "upload failed: $ERROR"
         return $ERR_FATAL
     fi
 
@@ -172,6 +175,7 @@ rapidshare_delete() {
     eval "$(process_options rapidshare "$MODULE_RAPIDSHARE_DELETE_OPTIONS" "$@")"
 
     local URL="$1"
+    local USER PASSWORD FILEID RESPONSE
 
     if ! test "$AUTH"; then
         log_error "Anonymous users cannot delete files"
