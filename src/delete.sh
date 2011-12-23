mcrapet/plowshare

commit 6cc4961cd733fb017623c5739f2f7ad526045217

delete.sh: move "file not found or already deleted" message from module to to script
When xxx_delete() function return $ERR_LINK_DEAD.

Modified modules:
- 2shared
- filesonic
- zshare

src/delete.sh
@@ -119,9 +119,15 @@ for URL in "$@"; do
 
     FUNCTION=${MODULE}_delete
     log_notice "Starting delete ($MODULE): $URL"
-    $FUNCTION "${UNUSED_OPTIONS[@]}" "$URL" && \
-        log_notice "File removed successfully" || \
-        RETVALS=(${RETVALS[@]} "$?")
+
+    if [ $DRETVAL -eq 0 ]; then
+        log_notice "File removed successfully"
+    else
+        RETVALS=(${RETVALS[@]} "$DRETVAL")
+        if [ $DRETVAL -eq $ERR_LINK_DEAD ]; then
+            log_error "Not found or already deleted"
+        fi
+    fi
 done
 
 if [ ${#RETVALS[@]} -eq 0 ]; then

src/modules/2shared.sh
@@ -101,7 +101,6 @@ MODULE_2SHARED_DELETE_OPTIONS=""
     ADMIN_PAGE=$(curl -c "$COOKIEFILE" "$URL") || return
 
     if ! match 'Delete File' "$ADMIN_PAGE"; then
-        log_error "File not found"
         rm -f "$COOKIEFILE"
         return $ERR_LINK_DEAD
     fi
@@ -117,7 +116,7 @@ MODULE_2SHARED_DELETE_OPTIONS=""
 
     curl -b "$COOKIEFILE" --referer "$URL" -o /dev/null \
         --data "resultMode=2&password=&description=&publisher=&downloadLink=${DL_LINK}&adminLink=${AD_LINK}" \
-        "$URL"
+        "$URL" || return
     # Can't parse for success, we get redirected to main page
 
     rm -f "$COOKIEFILE"

src/modules/filesonic.sh
@@ -338,7 +338,6 @@ filesonic_delete() {
         log_debug "Delete error"
         return $ERR_FATAL
     elif match 'Item not found' "$DELETE"; then
-        log_error "Not found or already deleted"
         return $ERR_LINK_DEAD
     fi
 

src/modules/zshare.sh
@@ -133,7 +133,6 @@ zshare_delete() {
     DELETE_PAGE=$(curl -L "$URL") || return
 
     if matchi 'File Not Found' "$DELETE_PAGE"; then
-        log_error "File not found"
         return $ERR_LINK_DEAD
     else
         FORM_KILLCODE=$(echo "$DELETE_PAGE" | parse_form_input_by_name "killCode")

commit 309728e47079d89ff24a0f4f6826abf8d0567d3b

plowdel: regression fix (was not working at all)
Regression introduced in revision 6cc4961cd733fb017623c5739f2f7ad526045217.

src/delete.sh
@@ -102,13 +102,14 @@ set_exit_trap
 
 RETVALS=()
 for URL in "$@"; do
+
     if [ -z "$URL" ]; then
         log_debug "empty argument, skipping"
         continue
     fi
 
     MODULE=$(get_module "$URL" "$MODULES")
-    if test -z "$MODULE"; then
+    if [ -z "$MODULE" ]; then
         log_error "Skip: no module for URL ($URL)"
         RETVALS=(${RETVALS[@]} $ERR_NOMODULE)
         continue
@@ -119,6 +120,9 @@ for URL in "$@"; do
 
     FUNCTION=${MODULE}_delete
     log_notice "Starting delete ($MODULE): $URL"
+
+    $FUNCTION "${UNUSED_OPTIONS[@]}" "$URL"
+    DRETVAL=$?
 
     if [ $DRETVAL -eq 0 ]; then
         log_notice "File removed successfully"