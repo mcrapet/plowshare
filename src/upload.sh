mcrapet/plowshare

commit 4fbfd9a803ce09d04bbf3edc1b2c14dad356e878

upload.sh: change output of module upload function
End of upload function where echoing download URL (with or without delete URL).
It was defined like this:

<code>
echo "$DL_URL"
</code>

or (if a delete url was available):

<code>
echo "$DL_URL ($DEL_URL)"
</code>

Now return results like download module function (1 result per line):

<code>
echo "$DL_URL"
echo "$DEL_URL"
echo "$ADMIN_URL_OR_CODE"
</code>

$DEL_URL or $ADMIN_URL_OR_CODE can be empty of even not echoed (memo: most important URL first!).

Presentation format: "%s (%s)" will be done in upload.sh.

Updated modules (10):
- 1fichier
- 2shared
- bayfiles
- depositfiles
- dl.free.fr
- fileserve
- megashares
- netload_in
- sendspace
- zshare

src/upload.sh
@@ -235,9 +235,9 @@ for FILE in "$@"; do
 
     TRY=0
     while :; do
-        : > "$UPCOOKIE"
+        :> "$UPCOOKIE"
         URETVAL=0
-        $FUNCTION "${UNUSED_OPTIONS[@]}" "$UPCOOKIE" "$LOCALFILE" "$DESTFILE" || URETVAL=$?
+        URESULT=$($FUNCTION "${UNUSED_OPTIONS[@]}" "$UPCOOKIE" "$LOCALFILE" "$DESTFILE") || URETVAL=$?
 
         (( ++TRY ))
         if [[ "$MAXRETRIES" -eq 0 ]]; then
@@ -252,7 +252,25 @@ for FILE in "$@"; do
         log_notice "Starting upload ($MODULE): retry ${TRY}/$MAXRETRIES"
     done
 
-    if [ $URETVAL -eq $ERR_LOGIN_FAILED ]; then
+    if [ $URETVAL -eq 0 ]; then
+        { read DL_URL; read DEL_URL; read ADMIN_URL_OR_CODE; } <<< "$URESULT" || true
+        if [ -n "$DL_URL" ]; then
+            if [ -n "$DEL_URL" ]; then
+                if [ -n "$ADMIN_URL_OR_CODE" ]; then
+                    echo "$DL_URL ($DEL_URL) ($ADMIN_URL_OR_CODE)"
+                else
+                    echo "$DL_URL ($DEL_URL)"
+                fi
+            elif [ -n "$ADMIN_URL_OR_CODE" ]; then
+                echo "$DL_URL ($ADMIN_URL_OR_CODE)"
+            else
+                echo "$DL_URL"
+            fi
+        else
+            log_error "Output URL expected"
+            URETVAL=$ERR_FATAL
+        fi
+    elif [ $URETVAL -eq $ERR_LOGIN_FAILED ]; then
         log_error "Login process failed. Bad username/password or unexpected content"
     fi
     RETVALS=(${RETVALS[@]} "$URETVAL")

src/modules/1fichier.sh
@@ -109,32 +109,15 @@ MODULE_1FICHIER_UPLOAD_REMOTE_SUPPORT=no
     REMOVE_ID=$(echo "$RESPONSE" | nth_line 4)
     DOMAIN_ID=$(echo "$RESPONSE" | nth_line 5)
 
-    case "$DOMAIN_ID" in
-        0)  echo -e "http://$DOWNLOAD_ID.1fichier.com (http://www.1fichier.com/remove/$DOWNLOAD_ID/$REMOVE_ID)"
-            ;;
-        1)  echo -e "http://$DOWNLOAD_ID.alterupload.com (http://www.alterupload.com/remove/$DOWNLOAD_ID/$REMOVE_ID)"
-            ;;
-        2)  echo -e "http://$DOWNLOAD_ID.cjoint.net (http://www.cjoint.net/remove/$DOWNLOAD_ID/$REMOVE_ID)"
-            ;;
-        3)  echo -e "http://$DOWNLOAD_ID.desfichiers.com (http://www.desfichiers.com/remove/$DOWNLOAD_ID/$REMOVE_ID)"
-            ;;
-        4)  echo -e "http://$DOWNLOAD_ID.dfichiers.com (http://www.dfichiers.com/remove/$DOWNLOAD_ID/$REMOVE_ID)"
-            ;;
-        5)  echo -e "http://$DOWNLOAD_ID.megadl.fr (http://www.megadl.fr/remove/$DOWNLOAD_ID/$REMOVE_ID)"
-            ;;
-        6)  echo -e "http://$DOWNLOAD_ID.mesfichiers.net (http://www.mesfichiers.net/remove/$DOWNLOAD_ID/$REMOVE_ID)"
-            ;;
-        7)  echo -e "http://$DOWNLOAD_ID.piecejointe.net (http://www.piecejointe.net/remove/$DOWNLOAD_ID/$REMOVE_ID)"
-            ;;
-        8)  echo -e "http://$DOWNLOAD_ID.pjointe.com (http://www.pjointe.com/remove/$DOWNLOAD_ID/$REMOVE_ID)"
-            ;;
-        9)  echo -e "http://$DOWNLOAD_ID.tenvoi.com (http://www.tenvoi.com/remove/$DOWNLOAD_ID/$REMOVE_ID)"
-            ;;
-        10) echo -e "http://$DOWNLOAD_ID.dl4free.com (http://www.dl4free.com/remove/$DOWNLOAD_ID/$REMOVE_ID)"
-            ;;
-        *)  log_error "Bad domain ID response, maybe API updated?"
-            return $ERR_FATAL
-            ;;
-    esac
-    return 0
+    local -a DOMAIN_STR=('1fichier.com' 'alterupload.com' 'cjoint.net' 'desfichiers.com' \
+        'dfichiers.com' 'megadl.fr' 'mesfichiers.net' 'piecejointe.net' 'pjointe.com' \
+        'tenvoi.com' 'dl4free.com' )
+
+    if [[ "$DOMAIN_ID" -gt 10 && "$DOMAIN_ID" -lt 0 ]]; then
+        log_error "Bad domain ID response, maybe API updated?"
+        return $ERR_FATAL
+    fi
+
+    echo "http://${DOWNLOAD_ID}.${DOMAIN_STR[$DOMAIN_ID]}"
+    echo "http://www.${DOMAIN_STR[$DOMAIN_ID]}/remove/$DOWNLOAD_ID/$REMOVE_ID"
 }

src/modules/2shared.sh
@@ -86,7 +86,9 @@ MODULE_2SHARED_DELETE_OPTIONS=""
     FILE_URL=$(echo "$DATA" | parse 'name="downloadLink"' "\(http:[^<]*\)") || return
     FILE_ADMIN=$(echo "$DATA" | parse 'name="adminLink"' "\(http:[^<]*\)")
 
-    echo "$FILE_URL ($FILE_ADMIN)"
+    echo "$FILE_URL"
+    echo
+    echo "$FILE_ADMIN"
 }
 
 # Delete a file uploaded to 2shared

src/modules/bayfiles.sh
@@ -26,8 +26,7 @@ MODULE_BAYFILES_DOWNLOAD_RESUME=yes
 MODULE_BAYFILES_DOWNLOAD_FINAL_LINK_NEEDS_COOKIE=no
 
 MODULE_BAYFILES_UPLOAD_OPTIONS="
-AUTH_FREE,b:,auth-free:,USER:PASSWORD,Free account
-PRINT_ALL_LINKS,,print-all-links,,Print admin and delete links next to the download link"
+AUTH_FREE,b:,auth-free:,USER:PASSWORD,Free account"
 MODULE_BAYFILES_UPLOAD_REMOTE_SUPPORT=no
 
 # Static function. Proceed with login (free-user or premium)
@@ -161,17 +160,15 @@ bayfiles_upload() {
         parse 'downloadUrl' 'downloadUrl":"\([^"]*\)' |\
         replace '\/' '/') || return
 
-    if test -n "$PRINT_ALL_LINKS"; then
-        ADMIN_URL=$(echo "$UPLOADED_FILE_JSON_DATA" |\
-            parse 'linksUrl' 'linksUrl":"\([^"]*\)' |\
-            replace '\/' '/') || return
+    DELETE_URL=$(echo "$UPLOADED_FILE_JSON_DATA" |\
+        parse 'deleteUrl' 'deleteUrl":"\([^"]*\)' |\
+        replace '\/' '/') || return
 
-        DELETE_URL=$(echo "$UPLOADED_FILE_JSON_DATA" |\
-            parse 'deleteUrl' 'deleteUrl":"\([^"]*\)' |\
-            replace '\/' '/') || return
+    ADMIN_URL=$(echo "$UPLOADED_FILE_JSON_DATA" |\
+        parse 'linksUrl' 'linksUrl":"\([^"]*\)' |\
+        replace '\/' '/') || return
 
-        echo "$URL ($ADMIN_URL) ($DELETE_URL)"
-    else
-        echo "$URL"
-    fi
+    echo "$URL"
+    echo "$DELETE_URL"
+    echo "$ADMIN_URL"
 }

src/modules/depositfiles.sh
@@ -239,7 +239,8 @@ depositfiles_upload() {
     DL_LINK=$(echo "$DATA" | parse 'ud_download_url[[:space:]]' "'\([^']*\)'") || return
     DEL_LINK=$(echo "$DATA" | parse 'ud_delete_url' "'\([^']*\)'") || return
 
-    echo "$DL_LINK ($DEL_LINK)"
+    echo "$DL_LINK"
+    echo "$DEL_LINK"
 }
 
 # Delete a file on depositfiles

src/modules/dl_free_fr.sh
@@ -65,7 +65,7 @@ dl_free_fr_upload() {
     local FILE="$2"
     local DESTFILE="$3"
     local UPLOADURL='http://dl.free.fr'
-    local PAGE FORM ACTION SESSIONID H STATUS MON_PL WAITTIME
+    local PAGE FORM ACTION SESSIONID H STATUS MON_PL WAITTIME DL RM
 
     log_debug "downloading upload page: $UPLOADURL"
     PAGE=$(curl "$UPLOADURL") || return
@@ -100,7 +100,7 @@ dl_free_fr_upload() {
 
     WAITTIME=5
     while [ $WAITTIME -lt 320 ] ; do
-        PAGE=$(curl "$MON_PL")
+        PAGE=$(curl "$MON_PL") || return
 
         if match 'En attente de traitement...' "$PAGE"; then
             log_debug "please wait"
@@ -120,7 +120,9 @@ dl_free_fr_upload() {
                     "window\.open('\(http:\/\/dl.free.fr\/[^?]*\)')" | html_to_utf8)
             RM=$(echo "$PAGE" | parse 'en ligne' \
                     "window\.open('\(http:\/\/dl.free.fr\/rm\.pl[^']*\)" | html_to_utf8)
-            echo "$DL ($RM)"
+
+            echo "$DL"
+            echo "$RM"
             return 0
         else
             log_error "unknown state, abort"

src/modules/fileserve.sh
@@ -268,8 +268,8 @@ fileserve_upload() {
     ID_DEL=$(echo "$PAGE" | parse_quiet 'deleteCode' 'deleteCode":"\([^"]*\)') || return
     FILENAME=$(echo "$PAGE" | parse_quiet 'fileName' 'fileName":"\([^"]*\)') || return
 
-    echo "$BASEURL/file/$ID/$FILENAME ($BASEURL/file/$ID/delete/$ID_DEL)"
-    return 0
+    echo "$BASEURL/file/$ID/$FILENAME"
+    echo "$BASEURL/file/$ID/delete/$ID_DEL"
 }
 
 # List a fileserve public folder URL

src/modules/megashares.sh
@@ -212,5 +212,6 @@ megashares_upload() {
     # <dt>Delete Link (keep this in a safe place):</dt>
     DEL_LINK=$(echo "$PAGE" | parse_tag '?dl=' a)
 
-    echo "$DL_LINK ($DEL_LINK)"
+    echo "$DL_LINK"
+    echo "$DEL_LINK"
 }

commit 2aedb33abbfa609e65a701e38d86112368853fdf

plowup: do not call download function in a subshell anymore
This was wrongly re-introduced in
revision 4fbfd9a803ce09d04bbf3edc1b2c14dad356e878

src/upload.sh
@@ -171,7 +171,8 @@ FUNCTION=${MODULE}_upload
 shift 1
 
 RETVALS=()
-UPCOOKIE=$(create_tempfile)
+UCOOKIE=$(create_tempfile)
+URESULT=$(create_tempfile)
 
 # Get configuration file module options
 test -z "$NO_PLOWSHARERC" && \
@@ -235,9 +236,9 @@ for FILE in "$@"; do
 
     TRY=0
     while :; do
-        :> "$UPCOOKIE"
+        :> "$UCOOKIE"
         URETVAL=0
-        URESULT=$($FUNCTION "${UNUSED_OPTIONS[@]}" "$UPCOOKIE" "$LOCALFILE" "$DESTFILE") || URETVAL=$?
+        $FUNCTION "${UNUSED_OPTIONS[@]}" "$UCOOKIE" "$LOCALFILE" "$DESTFILE" >"$URESULT" || URETVAL=$?
 
         (( ++TRY ))
         if [[ "$MAXRETRIES" -eq 0 ]]; then
@@ -253,7 +254,7 @@ for FILE in "$@"; do
     done
 
     if [ $URETVAL -eq 0 ]; then
-        { read DL_URL; read DEL_URL; read ADMIN_URL_OR_CODE; } <<< "$URESULT" || true
+        { read DL_URL; read DEL_URL; read ADMIN_URL_OR_CODE; } <"$URESULT" || true
         if [ -n "$DL_URL" ]; then
             if [ -n "$DEL_URL" ]; then
                 if [ -n "$ADMIN_URL_OR_CODE" ]; then
@@ -276,7 +277,7 @@ for FILE in "$@"; do
     RETVALS=(${RETVALS[@]} "$URETVAL")
 done
 
-rm -f "$UPCOOKIE"
+rm -f "$UCOOKIE" "$URESULT"
 
 if [ ${#RETVALS[@]} -eq 0 ]; then
     exit 0