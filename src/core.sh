mcrapet/plowshare

commit 78e7e116f6a59fe0ec7773c76216caa10173224c

core.sh: parse functions small enhancement (parse_attr)
It concerns parse_all_attr, parse_all_attr_quiet, parse_attr, parse_attr_quiet functions.

1. Like parse_tag functions, allow one single argument (instead of using '.' filtering regexp)

IMG='<img href="http://foo.com/bar.jpg" id = "image_id" >'
ID=$(echo "$IMG" | parse_attr 'id') || return

Note: "parse_attr b" is equivalent to "parse_attr . b" and "parse_attr b b".

2. Deal with unquoted attribute-value

IMG='<img href="http://foo.com/bar.jpg" id=image_id>'
ID=$(echo "$IMG" | parse_attr 'id') || return

3. Misc change:
- updated documentation (function header comments) of parse_all_tag function

src/core.sh
@@ -431,6 +431,7 @@ break_html_lines_alt() {
 # Notes:
 # - beginning and ending tag are on the same line
 # - this is non greedy, first occurrence is taken
+# - this is case sensitive, this should not
 # - "parse_xxx tag" is a shortcut for "parse_xxx tag tag"
 #
 # $1: regexp to filter (take lines matching $1 pattern)
@@ -441,7 +442,7 @@ parse_all_tag() {
     local T=${2:-"$1"}
     local STRING=$(sed -ne "/$1/s/<\/$T>.*$//p" | sed -e "s/^.*<$T\(>\|[[:space:]][^>]*>\)//")
     test "$STRING" && echo "$STRING" ||
-        { log_error "parse_tag failed: sed -n \"/$1/s/<$T>\""; return $ERR_FATAL; }
+        { log_error "parse_tag failed (sed): \"/$1/ <$T>\""; return $ERR_FATAL; }
 }
 
 # Like parse_all_tag, but hide possible error
@@ -462,14 +463,33 @@ parse_tag_quiet() {
 # Parse HTML attribute content
 # http://www.w3.org/TR/html-markup/syntax.html#syntax-attributes
 # Note:
-# - Do not deal with unquoted attribute-value syntax
+# - empty attribute syntax is not supported (ex: <input disabled>)
+# - this is greedy, last occurrence is taken
+# - this is case sensitive, this should not
+# - "parse_xxx attr" is a shortcut for "parse_xxx attr attr"
 #
 # $1: regexp to filter (take lines matching $1 pattern)
 # $2: attribute name. Example: "href".
 # stdin: (X)HTML data
 # stdout: result
 parse_all_attr() {
-    parse_all "$1" "$2[[:space:]]*=[[:space:]]*[\"']\?\([^\"'>]*\)"
+    local A=${2:-"$1"}
+    local STRING=$(sed -ne "/$1/s/.*$A[[:space:]]*=[[:space:]]*\([\"']\?[^\"'>]*\).*/\1/p")
+    if [[ ${#STRING} > 1 ]]; then
+        if [ '"' = "${STRING:0:1}" -o "'" = "${STRING:0:1}" ]; then
+            echo "${STRING:1}"
+        else
+            echo "${STRING%%[	 ]*}"
+        fi
+        return 0
+    # unquoted attribute with a single character
+    elif [[ ${#STRING} = 1 && ${STRING:0:1} != '"' && ${STRING:0:1} != "'" ]]; then
+        echo "$STRING"
+        return 0
+    fi
+
+    log_error "parse_attr failed (sed): \"/$1/ $A=\""
+    return $ERR_FATAL
 }
 
 # Like parse_all_attr, but hide possible error

commit 19d907997ab0f87015bf8d90479b31039acbfd0f

core.sh: parse functions small enhancement (parse_attr) [FIX]
This fix regression introduced in previous
revision 78e7e116f6a59fe0ec7773c76216caa10173224c

It concerns parse_all_attr, parse_all_attr_quiet, parse_attr, parse_attr_quiet
functions.

1. Like parse_tag functions, allow one single argument (instead of using '.'
filtering regexp)

IMG='<img href="http://foo.com/bar.jpg" id = "image_id" >'
ID=$(echo "$IMG" | parse_attr 'id') || return

Note: "parse_attr b" is equivalent to "parse_attr . b" and "parse_attr b b".

2. Deal with unquoted attribute-value

IMG='<img href="http://foo.com/bar.jpg" id=image_id>'
ID=$(echo "$IMG" | parse_attr 'id') || return

3. Misc change:
- parse_all_tag function, minor refactor

src/core.sh
@@ -441,8 +441,13 @@ break_html_lines_alt() {
 parse_all_tag() {
     local T=${2:-"$1"}
     local STRING=$(sed -ne "/$1/s/<\/$T>.*$//p" | sed -e "s/^.*<$T\(>\|[[:space:]][^>]*>\)//")
-    test "$STRING" && echo "$STRING" ||
-        { log_error "parse_tag failed (sed): \"/$1/ <$T>\""; return $ERR_FATAL; }
+
+    if [ -z "$STRING" ]; then
+        log_error "parse_tag failed (sed): \"/$1/ <$T>\""
+        return $ERR_FATAL
+    fi
+
+    echo "$STRING"
 }
 
 # Like parse_all_tag, but hide possible error
@@ -474,22 +479,16 @@ parse_tag_quiet() {
 # stdout: result
 parse_all_attr() {
     local A=${2:-"$1"}
-    local STRING=$(sed -ne "/$1/s/.*$A[[:space:]]*=[[:space:]]*\([\"']\?[^\"'>]*\).*/\1/p")
-    if [[ ${#STRING} > 1 ]]; then
-        if [ '"' = "${STRING:0:1}" -o "'" = "${STRING:0:1}" ]; then
-            echo "${STRING:1}"
-        else
-            echo "${STRING%%[	 ]*}"
-        fi
-        return 0
-    # unquoted attribute with a single character
-    elif [[ ${#STRING} = 1 && ${STRING:0:1} != '"' && ${STRING:0:1} != "'" ]]; then
-        echo "$STRING"
-        return 0
+    local STRING=$(sed \
+        -ne "/$1/s/.*$A[[:space:]]*=[[:space:]]*[\"']\([^\"'>]*\).*/\1/p" \
+        -ne "/$1/s/.*$A[[:space:]]*=[[:space:]]*\([^\"'<=> 	]\+\).*/\1/p")
+
+    if [ -z "$STRING" ]; then
+        log_error "parse_attr failed (sed): \"/$1/ $A=\""
+        return $ERR_FATAL
     fi
 
-    log_error "parse_attr failed (sed): \"/$1/ $A=\""
-    return $ERR_FATAL
+    echo "$STRING"
 }
 
 # Like parse_all_attr, but hide possible error