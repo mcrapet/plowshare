1a71ad2aaf655b9b55611276287c5170a6889409
Matthieu Crapet <mcrapet@gmail.com>
2012-05-17 14:35:11 +0200

    extabit: add free account download & upload

--- src/modules/extabit.sh
+++ src/modules/extabit.sh
@@ -20,9 +20,35 @@
 
 MODULE_EXTABIT_REGEXP_URL="http://\(www\.\)\?extabit\.com/file/"
 
-MODULE_EXTABIT_DOWNLOAD_OPTIONS=""
+MODULE_EXTABIT_DOWNLOAD_OPTIONS="
+AUTH,a:,auth:,EMAIL:PASSWORD,User account"
 MODULE_EXTABIT_DOWNLOAD_RESUME=no
 MODULE_EXTABIT_DOWNLOAD_FINAL_LINK_NEEDS_COOKIE=no
+
+MODULE_EXTABIT_UPLOAD_OPTIONS="
+AUTH,a:,auth:,EMAIL:PASSWORD,User account (mandatory)"
+MODULE_EXTABIT_UPLOAD_REMOTE_SUPPORT=no
+
+# Static function. Proceed with login (free or premium)
+extabit_login() {
+    local AUTH=$1
+    local COOKIE_FILE=$2
+    local BASE_URL=$3
+    local LOGIN_DATA LOGIN_RESULT STATUS
+
+    LOGIN_DATA='email=$USER&pass=$PASSWORD&remember=1&auth_submit_login.x=33&auth_submit_login.y=5'
+    LOGIN_RESULT=$(post_login "$AUTH" "$COOKIE_FILE" "$LOGIN_DATA" \
+        "$BASE_URL/login.jsp" -b 'language=en' -L) || return
+
+    # If successful, two entries are added into cookie file: auth_uid and auth_hash
+    STATUS=$(parse_cookie_quiet 'auth_hash' < "$COOKIE_FILE")
+    if [ -z "$STATUS" ]; then
+        return $ERR_LOGIN_FAILED
+    fi
+
+    match 'Buy premium' "$LOGIN_RESULT" && \
+        echo 'free' || echo 'premium'
+}
 
 # Output an extabit.com file download URL
 # $1: cookie file
@@ -34,9 +60,24 @@ extabit_download() {
     local COOKIE_FILE=$1
     local URL=$2
     local BASE_URL='http://extabit.com'
-    local PAGE DIV_ID WAIT_TIME JSON ERR QUERY FILE_NAME FILE_URL
+    local REDIR PAGE DIV_ID WAIT_TIME JSON ERR QUERY FILE_NAME FILE_URL TYPE
 
-    PAGE=$(curl -c "$COOKIE_FILE" -b 'language=en' "$URL") || return
+    REDIR=$(curl -i "$URL" | grep_http_header_location_quiet)
+    [ -n "$REDIR" ] && URL=$REDIR
+
+    if [ -n "$AUTH" ]; then
+        TYPE=$(extabit_login "$AUTH" "$COOKIE_FILE" "$BASE_URL") || return
+        log_debug "Account type: $TYPE"
+
+        if [ "$TYPE" != 'free' ]; then
+            log_error "FIXME: not yet implemented!"
+            return $ERR_FATAL
+        fi
+
+        PAGE=$(curl -b "$COOKIE_FILE" -b 'language=en' "$URL") || return
+    else
+        PAGE=$(curl -c "$COOKIE_FILE" -b 'language=en' "$URL") || return
+    fi
 
     # <h1>File not found</h1>
     if match 'page_404_header' "$PAGE"; then
@@ -75,6 +116,10 @@ extabit_download() {
         -d "capture=$WORD" \
         "$URL") || return
 
+    # Sanity check
+    [ -z "$JSON" ] && \
+        log_error "Bad state. Empty answer"
+
     #{"err":"Entered digits are incorrect."}
     #{"ok":true,"href":"?af"}
     if ! match_json_true 'ok' "$JSON"; then
@@ -97,4 +142,46 @@ extabit_download() {
 
     echo "$FILE_URL"
     echo "$FILE_NAME"
+}
+
+# Upload a file to extabit.com
+# $1: cookie file
+# $2: input file (with full path)
+# $3: remote filename
+# stdout: extabit.com download link
+extabit_upload() {
+    eval "$(process_options extabit "$MODULE_EXTABIT_UPLOAD_OPTIONS" "$@")"
+
+    local COOKIE_FILE=$1
+    local FILE=$2
+    local DESTFILE=$3
+    local BASE_URL='http://extabit.com'
+    local PAGE
+
+    test "$AUTH" || return $ERR_LINK_NEED_PERMISSIONS
+
+    # Official API is quite incomplete.
+    # http://code.google.com/p/extabit-api/wiki/APIDocumentation
+
+    extabit_login "$AUTH" "$COOKIE_FILE" "$BASE_URL" > /dev/null || return
+    PAGE=$(curl -L -b "$COOKIE_FILE" -b 'language=en' "$BASE_URL") || return
+
+    local FORM_HTML FORM_ACTION FORM_MAXFSIZE
+    FORM_HTML=$(grep_form_by_id "$PAGE" 'upload_files_form') || return
+    FORM_ACTION=$(echo "$FORM_HTML" | parse_form_action) || return
+    FORM_MAXFSIZE=$(echo "$FORM_HTML" | parse_form_input_by_name 'MAX_FILE_SIZE')
+
+    local SZ=$(get_filesize "$FILE")
+    if [ "$SZ" -gt "$FORM_MAXFSIZE" ]; then
+        log_debug "file is bigger than $FORM_MAXFSIZE"
+        return $ERR_SIZE_LIMIT_EXCEEDED
+    fi
+
+    PAGE=$(curl_with_log -L -b "$COOKIE_FILE" \
+        -F "MAX_FILE_SIZE=$FORM_MAXFSIZE"    \
+        -F "my_file=@$FILE;filename=$DESTFILE" \
+        -F 'checkbox_terms=on' \
+        "$FORM_ACTION") || return
+
+    echo "$PAGE" | parse_attr 'df_html_link' 'href' || return
 }
