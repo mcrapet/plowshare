a5941e4bdb35f4c8b7dc012b907ea1cd2812a8e8
Matthieu Crapet <mcrapet@gmail.com>
2012-05-27 12:37:02 +0200

    115, depositfiles, uploading.com: don't use \w in site regexp, this is not POSIX
    Using [[:alnum:]] is better (which do not include underscore character).

--- src/modules/depositfiles.sh
+++ src/modules/depositfiles.sh
@@ -18,7 +18,7 @@
 # You should have received a copy of the GNU General Public License
 # along with Plowshare.  If not, see <http://www.gnu.org/licenses/>.
 
-MODULE_DEPOSITFILES_REGEXP_URL="http://\(\w\+\.\)\?depositfiles\.com/"
+MODULE_DEPOSITFILES_REGEXP_URL="http://\([[:alnum:]]\+\.\)\?depositfiles\.com/"
 
 MODULE_DEPOSITFILES_DOWNLOAD_OPTIONS="
 AUTH,a:,auth:,USER:PASSWORD,User account"
--- src/modules/uploading.sh
+++ src/modules/uploading.sh
@@ -18,7 +18,7 @@
 # You should have received a copy of the GNU General Public License
 # along with Plowshare.  If not, see <http://www.gnu.org/licenses/>.
 
-MODULE_UPLOADING_REGEXP_URL="http://\(\w\+\.\)\?uploading\.com/"
+MODULE_UPLOADING_REGEXP_URL="http://\([[:alnum:]]\+\.\)\?uploading\.com/"
 
 MODULE_UPLOADING_DOWNLOAD_OPTIONS=""
 MODULE_UPLOADING_DOWNLOAD_RESUME=no
