63e945d45a592bd62a12ff259286d78577ca161c
Matthieu Crapet <mcrapet@gmail.com>
2012-05-30 10:35:19 +0200

    core.sh: add comment for "set -o pipefail"
    Comment taken from crosstool-ng (scripts/functions).

--- src/core.sh
+++ src/core.sh
@@ -18,6 +18,7 @@
 # You should have received a copy of the GNU General Public License
 # along with Plowshare.  If not, see <http://www.gnu.org/licenses/>.
 
+# Make pipes fail on the first failed command (requires Bash 3+)
 set -o pipefail
 
 # Global error codes
