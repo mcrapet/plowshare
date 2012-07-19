87c408068cc68c41bc634420d34c3d93b3567259
Jan <krompospeed@googlemail.com>
2012-07-19 17:18:38 +0200

    divshare (upload): add extended options (description, folder, email)

--- src/modules/divshare.sh
+++ src/modules/divshare.sh
@@ -25,7 +25,10 @@ MODULE_DIVSHARE_DOWNLOAD_RESUME=no
 MODULE_DIVSHARE_DOWNLOAD_FINAL_LINK_NEEDS_COOKIE=yes
 
 MODULE_DIVSHARE_UPLOAD_OPTIONS="
-AUTH_FREE,b,auth-free,a=EMAIL:PASSWORD,Free account (mandatory)"
+AUTH_FREE,b,auth-free,a=EMAIL:PASSWORD,Free account (mandatory)
+DESCRIPTION,d,description,S=DESCRIPTION,Set file description
+FOLDER,,folder,s=FOLDER,Folder to upload files into
+TOEMAIL,,email-to,e=EMAIL,<To> field for notification email"
 MODULE_DIVSHARE_UPLOAD_REMOTE_SUPPORT=no
 
 
@@ -50,6 +53,54 @@ divshare_login() {
         '^[[:space:]]*logged in as \(.\+\) |') || return
 
     log_debug "Successfully logged in as member '$NAME'"
+}
+
+# Check if specified folder name is valid.
+# When multiple folders wear the same name, first one is taken.
+# $1: source code of main page
+# $2: folder name selected by user
+# stdout: folder ID
+divshare_check_folder() {
+    local -r PAGE=$1
+    local -r NAME=$2
+    local LINES FOLDERS FOL
+
+    # - Extract the line containing all <option> tags for folders
+    # - Split them so we have one tag per line for later parsing
+    LINES=$(echo "$PAGE" | parse '<select name="gallery_id"' \
+        '^[[:space:]]*\(.*\)[[:space:]]*$' 2 | break_html_lines) || return
+
+    # <option value="ID">NAME</option>
+    FOLDERS=$(echo "$LINES" | parse_all_tag option) || return
+    if [ -z "$FOLDERS" ]; then
+        log_error "No folder found, site updated?"
+        return $ERR_FATAL
+    fi
+
+    log_debug 'Available folders:' $FOLDERS
+
+    while IFS= read -r FOL; do
+        if [ "$FOL" = "$NAME" ]; then
+            echo "$LINES" | parse_attr "<option.*>$FOL</option>" 'value' || return
+            return 0
+        fi
+    done <<< "$FOLDERS"
+
+    log_error "Invalid folder, choose from:" $FOLDERS
+    return $ERR_BAD_COMMAND_LINE
+}
+
+# Extract file id from download link
+# $1: divshare url
+# stdout: file id
+divshare_extract_file_id() {
+    local FILE_ID
+
+    FILE_ID=$(echo "$1" | parse '.' \
+        'download/\([[:digit:]]\{8\}\)-[[:alnum:]]\{3\}$') || return
+    log_debug "File ID: '$FILE_ID'"
+
+    echo "$FILE_ID"
 }
 
 # Output a divshare file download URL
@@ -104,7 +155,9 @@ divshare_upload() {
     local -r FILE=$2
     local -r DEST_FILE=$3
     local -r BASE_URL='http://www.divshare.com'
-    local PAGE UP_URL SIZE MAX_SIZE LINK
+    local FOLDER_ID=0
+
+    local PAGE UP_URL FOLDER_OPT SIZE MAX_SIZE LINK
 
     test "$AUTH_FREE" || return $ERR_LINK_NEED_PERMISSIONS
     divshare_login "$AUTH_FREE" "$COOKIE_FILE" "$BASE_URL" || return
@@ -124,6 +177,15 @@ divshare_upload() {
         return $ERR_SIZE_LIMIT_EXCEEDED
     fi
 
+    # If user chose a folder, check it now
+    if [ -n "$FOLDER" ]; then
+        FOLDER_ID=$(divshare_check_folder "$PAGE" "$FOLDER") || return
+        FOLDER_OPT='-F gallery=on'
+    fi
+
+    log_debug "Upload URL: '$UP_URL'"
+    log_debug "Folder ID: '$FOLDER_ID'"
+
     # Upload file
     PAGE=$(curl_with_log -b "$COOKIE_FILE" \
         -F 'upload_method=php' -F "MAX_FILE_SIZE=$MAX_SIZE" \
@@ -137,8 +199,9 @@ divshare_upload() {
         -F 'file4_description=Description' \
         -F 'file5=;type=application/octet-stream;filename=' \
         -F 'file5_description=Description' \
-        -F 'gallery_id=0' -F 'gallery_title=' -F 'gallery_password=' \
-        -F 'email_to=' -F 'terms=on' "$UP_URL") || return
+        $FOLDER_OPT \
+        -F "gallery_id=$FOLDER_ID" -F 'gallery_title=' -F 'gallery_password=' \
+        -F "email_to=$TOEMAIL" -F 'terms=on' "$UP_URL") || return
 
     # <img src="http://divshare.com/images/v4/upload/files_uploaded_text.png">
     # Share them with this links.
@@ -151,7 +214,7 @@ divshare_upload() {
     elif match '<title>.*Upload Error</title>' "$PAGE"; then
 
         # Sorry, we can't upload a file of this type. Some files, particularly executable ones, pose a security risk to our server and our users.
-        if match "Sorry, we can't upload a file of this type." "$PAGE"; then
+        if match "Sorry, we can't upload a file of this type\." "$PAGE"; then
             log_error 'Banned file type. Try changing the extension.'
 
         # Sorry, we couldn't process the image "pic.jpg" &mdash; it may not be a valid JPG, GIF or PNG file.
@@ -169,6 +232,19 @@ divshare_upload() {
     else
         log_error 'Unexpected content. Site updated?'
         return $ERR_FATAL
+    fi
+
+    # Do we need to edit the file? (set description)
+    if [ -n "$DESCRIPTION" ]; then
+        local FILE_ID
+
+        FILE_ID=$(divshare_extract_file_id "$LINK") || return
+        PAGE=$(curl -b "$COOKIE_FILE" -F "desc_id=$FILE_ID" \
+            -F "description=$DESCRIPTION" -F 'v3=true' \
+            "$BASE_URL/scripts/ajax/description.php") || return
+
+        match "$DESCRIPTION" "$PAGE" || \
+            log_error 'Could not edit description. Site update?'
     fi
 
     # Output link and delete link (which is actually the same)
