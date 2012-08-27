b0243cb10b5b5808c72717ef2ddeb79efe7ff34f
Jan <krompospeed@googlemail.com>
2012-08-27 02:00:28 +0200

    uploaded_to (login, download): rewrite/update (upstream API change)
    - clean up copyright entry (YFTR: Krompo@speed.1s.fr is my old email)
    - move account type check into 'login'
    - introduce function 'uploaded_to_parse_json_alt' (parse flawed JSON)
    - better reuse of common code
    - cosmetics
    
    Bugfix:
    - proper handling of password protected files for premium users

--- src/modules/uploaded_to.sh
+++ src/modules/uploaded_to.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 #
 # uploaded.to module
-# Copyright (c) 2011 Krompo@speed.1s.fr
-# Copyright (c) 2012 Plowshare team
+# Copyright (c) 2011-2012 Plowshare team
 #
 # This file is part of Plowshare.
 #
@@ -36,162 +35,276 @@ MODULE_UPLOADED_TO_DELETE_OPTIONS="
 AUTH,a,auth,a=USER:PASSWORD,User account (mandatory)"
 MODULE_UPLOADED_TO_LIST_OPTIONS=""
 
-# Static function. Proceed with login (free-membership or premium)
+# Static function. Proceed with login
+# $1: authentication
+# $2: cookie file
+# $3: base url
+# stdout: account type ("free" or "premium") on success
 uploaded_to_login() {
-    local AUTH=$1
-    local COOKIE_FILE=$2
-    local BASEURL=$3
-
-    local LOGIN_DATA LOGIN_RESULT NAME
+    local -r AUTH=$1
+    local -r COOKIE_FILE=$2
+    local -r BASE_URL=$3
+    local LOGIN_DATA PAGE ERR TYPE ID NAME
 
     LOGIN_DATA='id=$USER&pw=$PASSWORD'
-    LOGIN_RESULT=$(post_login "$AUTH" "$COOKIE_FILE" "$LOGIN_DATA" \
-        "$BASEURL/io/login") || return
+    PAGE=$(post_login "$AUTH" "$COOKIE_FILE" "$LOGIN_DATA" \
+        "$BASE_URL/io/login") || return
 
-    # Note: "auth" entry is present in cookie too
-    NAME=$(parse_cookie_quiet 'login' < "$COOKIE_FILE")
-    if [ -n "$NAME" ]; then
-        NAME=$(echo "$NAME" | uri_decode | cut -d'&' -f2)
-        log_debug "Successfully logged in as ${NAME:3} member"
-        return 0
+    # Note: Cookies "login" + "auth" get set on successful login
+    ERR=$(echo "$PAGE" | parse_json_quiet err)
+
+    if [ -n "$ERR" ]; then
+        log_error "Remote error: $ERR"
+        return $ERR_LOGIN_FAILED
     fi
 
-    return $ERR_LOGIN_FAILED
+    # Note: Login changes site's language according to account's preference
+    uploaded_to_switch_lang "$COOKIE_FILE" "$BASE_URL" || return
+
+    # Determine account type
+    PAGE=$(curl -b "$COOKIE_FILE" "$BASE_URL/me") || return
+    ID=$(echo "$PAGE" | parse 'ID:' '<em.*>\(.*\)</em>' 1) || return
+    NAME=$(echo "$PAGE" | parse 'Alias:' '<b><b>\(.*\)</b></b>' 1) || return
+    TYPE=$(echo "$PAGE" | parse 'Status:' '<em>\(.*\)</em>' 1) || return
+
+    if [ "$TYPE" = 'Free' ]; then
+        TYPE='free'
+    elif [ "$TYPE" = 'Premium' ]; then
+        TYPE='premium'
+    else
+        log_error 'Could not determine account type. Site updated?'
+        return $ERR_FATAL
+    fi
+
+    log_debug "Successfully logged in as $TYPE member '$ID' ($NAME)"
+    echo "$TYPE"
 }
 
-# Output an uploaded.to file download URL
+# Switch language to english
+# $1: cookie file
+# $2: base URL
+uploaded_to_switch_lang() {
+    # Note: Language is associated with session, no new cookie is set
+    curl -b "$1" -o /dev/null "$2/language/en" || return
+}
+
+# Simple and limited parsing of flawed JSON
+#
+# Notes:
+# - Large parts copied from "parse_json" in core.sh (look there for further documentation)
+# - Also accepts flawed JSON (unquoted or single quoted names/strings)
+#
+# $1: variable name (string)
+# $2: (optional) preprocess option. Accepted values are:
+#     - "join": make a single line of input stream.
+#     - "split": split input buffer on comma character (,).
+# stdin: JSON data
+# stdout: result
+uploaded_to_parse_json_alt() {
+    local -r D="[\"']\?" # string/name delimiter
+    local -r S="^.*$D$1$D[[:space:]]*:[[:space:]]*" # start of JSON string
+    local -r E='\([,}[:space:]].*\)\?$' # end of JSON string
+    local STRING PRE
+
+    if [ "$2" = 'join' ]; then
+        PRE="tr -d '\n\r' |"
+    elif [ "$2" = 'split' ]; then
+        PRE="sed -e 's/,[[:space:]]*/\n/g' |"
+    fi
+
+    STRING=$($PRE sed -n \
+        -e "s/$S\(-\?\(0\|[1-9][[:digit:]]*\)\(\.[[:digit:]]\+\)\?\([eE][-+]\?[[:digit:]]\+\)\?\)$E/\1/p" \
+        -e "s/$S\(true\|false\|null\)$E/\1/p" \
+        -e "s/\\\\\"/\\\\q/g; s/$S$D\([^,}[:space:]\"']*\)$D$E/\1/p")
+
+    if [ -z "$STRING" ]; then
+        log_error "$FUNCNAME failed (json): \"$1\""
+        return $ERR_FATAL
+    fi
+
+    # Translate two-character sequence escape representations
+    STRING=${STRING//\\q/\"}
+    STRING=${STRING//\\\\/\\}
+    STRING=${STRING//\\\//\/}
+    STRING=${STRING//\\b/$'\b'}
+    STRING=${STRING//\\f/$'\f'}
+    STRING=${STRING//\\n/$'\n'}
+    STRING=${STRING//\\r/$'\r'}
+    STRING=${STRING//\\t/	}
+
+    echo "$STRING"
+}
+
+# Output an Uploaded.to file download URL
 # $1: cookie file
 # $2: upload.to url
 # stdout: real file download link
-# Note: Anonymous download restriction: 1 file every 60 minutes.
 uploaded_to_download() {
-    local -r COOKIEFILE=$1
+    local -r COOKIE_FILE=$1
     local -r BASE_URL='http://uploaded.net'
-    local URL FILE_ID HTML SLEEP FILE_NAME FILE_URL PAGE
+    local URL ACCOUNT PAGE JSON WAIT ERR FILE_ID FILE_NAME FILE_URL
 
-    # uploaded.to redirects all possible urls of a file to the canonical one
-    # ($URL result can have two lines before 'last_line')
+    # Uploaded.to redirects all possible urls of a file to the canonical one
+    # Note: There can be multiple redirections before the final one
     URL=$(curl -I -L "$2" | grep_http_header_location_quiet | last_line) || return
-    if test -z "$URL"; then
-        URL=$2
-    fi
+    [ -n "$URL" ] || URL=$2
 
-    # recognize folders
-    if match 'uploaded\.net/folder/' "$URL"; then
-        log_error "This is a directory list"
+    # Recognize folders
+    if match "$BASE_URL/folder/" "$URL"; then
+        log_error 'This is a directory list'
         return $ERR_FATAL
     fi
 
     # Page not found
     # The requested file isn't available anymore!
-    if match 'uploaded\.net/\(404\|410\)' "$URL"; then
+    if match "$BASE_URL/\(404\|410\)" "$URL"; then
         return $ERR_LINK_DEAD
     fi
 
-    test "$CHECK_LINK" && return 0
+    [ -n "$CHECK_LINK" ] && return 0
 
+    uploaded_to_switch_lang "$COOKIE_FILE" "$BASE_URL" || return
+
+    # Note: File owner never needs password and only owner may access private
+    # files, so login comes first.
     if [ -n "$AUTH" ]; then
-        uploaded_to_login "$AUTH" "$COOKIEFILE" "$BASE_URL" || return
-
-        # set website language to english
-        log_debug "must change account preferred language to english"
-        curl -b "$COOKIEFILE" "$BASE_URL/language/en" || return
-
-        # save HTTP headers to detect "direct downloads" option
-        HTML=$(curl -i -b "$COOKIEFILE" "$URL") || return
-
-        # Premium user ?
-        if ! match 'Choose your download method' "$HTML"; then
-            FILE_URL=$(echo "$HTML" | grep_http_header_location)
-            if [ -z "$FILE_URL" ]; then
-                FILE_URL=$(echo "$HTML" | parse_attr 'stor' 'action') || return
-            fi
-
-            FILE_NAME=$(curl --head -b "$COOKIEFILE" "$FILE_URL" | \
-                grep_http_header_content_disposition)
-
-            # Non premium cannot resume downloads
-            MODULE_UPLOADED_TO_DOWNLOAD_RESUME=yes
-
-            echo "$FILE_URL"
-            echo "$FILE_NAME"
-            return 0
-        fi
-    else
-        # set website language to english (currently default language)
-        curl -c "$COOKIEFILE" "$BASE_URL/language/en" || return
-
-        HTML=$(curl -b "$COOKIEFILE" "$URL") || return
+        ACCOUNT=$(uploaded_to_login "$AUTH" "$COOKIE_FILE" "$BASE_URL") || return
     fi
 
-    # extract the raw file id
-    FILE_ID=$(echo "$URL" | parse 'uploaded' '/file/\([^/]*\)')
-    log_debug "file id=$FILE_ID"
+    # Note: Save HTTP headers to catch premium users' "direct downloads"
+    PAGE=$(curl -i -b "$COOKIE_FILE" "$URL") || return
 
-    # check for files that need a password (file owner never needs password,
-    # so this comes after login)
-    if match '<h2>Authentification</h2>' "$HTML"; then
-        log_debug "File is password protected"
+    # Check for files that need a password
+    if match '<h2>Authentification</h2>' "$PAGE"; then
+        log_debug 'File is password protected'
+
         if [ -z "$LINK_PASSWORD" ]; then
             LINK_PASSWORD=$(prompt_for_password) || return
         fi
 
-        HTML=$(curl -b "$COOKIEFILE" -F "pw=$LINK_PASSWORD" "$URL") || return
-        if match '<h2>Authentification</h2>' "$HTML"; then
+        # Note: Again, consider "direct downloads"
+        PAGE=$(curl -i -b "$COOKIE_FILE" -F "pw=$LINK_PASSWORD" "$URL") || return
+
+        if match '<h2>Authentification</h2>' "$PAGE"; then
             return $ERR_LINK_PASSWORD_REQUIRED
         fi
     fi
 
-    # Our service is currently unavailable in your country. We are sorry about that.
-    if match '<h2>Not available</h2>' "$HTML"; then
-        echo 600
+    if [ "$ACCOUNT" = "premium" ]; then
+        # Premium users can resume downloads
+        MODULE_UPLOADED_TO_DOWNLOAD_RESUME=yes
+
+        # Get download link, if this was a direct download
+        FILE_URL=$(echo "$PAGE" | grep_http_header_location_quiet)
+
+        if [ -z "$FILE_URL" ]; then
+            FILE_URL=$(echo "$PAGE" | parse_attr 'stor' 'action') || return
+        fi
+
+        FILE_NAME=$(curl -I -b "$COOKIE_FILE" "$FILE_URL" | \
+            grep_http_header_content_disposition) || return
+
+        echo "$FILE_URL"
+        echo "$FILE_NAME"
+        return 0
+    fi
+
+    if match '^[[:space:]]*var free_enabled = false;' "$PAGE"; then
+        log_error 'No free download slots available'
+        echo 300 # wait some arbitrary time
         return $ERR_LINK_TEMP_UNAVAILABLE
     fi
 
-    # retrieve the waiting time
-    SLEEP=$(echo "$HTML" | parse '<span>Current waiting period' \
-        'period: <span>\([[:digit:]]\+\)</span>')
-    test -z "$SLEEP" && log_error "can't get sleep time" && \
-        log_debug "sleep time: $SLEEP" && return $ERR_FATAL
+    # Extract the raw file ID
+    FILE_ID=$(echo "$URL" | parse . '/file/\([^/]*\)') || return
 
-    wait $((SLEEP + 1)) seconds || return
+    # Request download (use dummy "-d" to force a POST request)
+    JSON=$(curl -b "$COOKIE_FILE" --referer "$URL" \
+        -H 'X-Requested-With: XMLHttpRequest' -d '' \
+        "$BASE_URL/io/ticket/slot/$FILE_ID") || return
+
+    if [ "$JSON" != '{succ:true}' ]; then
+        ERR=$(echo "$JSON" | parse_json_quiet 'err')
+
+        # from 'http://uploaded.to/js/download.js' - 'function(limit)'
+        if [ "$ERR" = 'limit-dl' ]; then
+            log_error 'Free download limit reached'
+            echo 600 # wait some arbitrary time
+            return $ERR_LINK_TEMP_UNAVAILABLE
+
+        elif [ "$ERR" = 'limit-parallel' ]; then
+            log_error 'No parallel download allowed.'
+            echo 600 # wait some arbitrary time
+            return $ERR_LINK_TEMP_UNAVAILABLE
+
+        elif [ "$ERR" = 'limit-size' ]; then
+            return $ERR_SIZE_LIMIT_EXCEEDED
+
+        elif [ "$ERR" = 'limit-slot' ]; then
+            log_error 'No free download slots available'
+            echo 300 # wait some arbitrary time
+            return $ERR_LINK_TEMP_UNAVAILABLE
+        fi
+
+        log_error "Unexpected remote error: $ERR"
+        return $ERR_FATAL
+    fi
+
+    # <span>Current waiting period: <span>30</span> seconds</span>
+    WAIT=$(echo "$PAGE" | parse '<span>Current waiting period' \
+        'period: <span>\([[:digit:]]\+\)</span>') || return
+    wait $((WAIT + 1)) || return
 
     # from 'http://uploaded.to/js/download.js' - 'Recaptcha.create'
     local PUBKEY WCI CHALLENGE WORD ID
     PUBKEY='6Lcqz78SAAAAAPgsTYF3UlGf2QFQCNuPMenuyHF3'
     WCI=$(recaptcha_process $PUBKEY) || return
-    { read WORD; read CHALLENGE; read ID; } <<<"$WCI"
+    { read WORD; read CHALLENGE; read ID; } <<< "$WCI"
 
-    local DATA="recaptcha_challenge_field=$CHALLENGE&recaptcha_response_field=$WORD"
-    PAGE=$(curl -b "$COOKIEFILE" --referer "$URL" \
-        --data "$DATA" "$BASE_URL/io/ticket/captcha/$FILE_ID") || return
-    log_debug "Captcha response: $PAGE"
+    JSON=$(curl -b "$COOKIE_FILE" --referer "$URL" \
+        -H 'X-Requested-With: XMLHttpRequest' \
+        -d "recaptcha_challenge_field=$CHALLENGE" \
+        -d "recaptcha_response_field=$WORD" \
+        "$BASE_URL/io/ticket/captcha/$FILE_ID") || return
 
-    # check for possible errors
-    if match 'captcha' "$PAGE"; then
-        captcha_nack $ID
-        return $ERR_CAPTCHA
-    elif match 'limit\|err' "$PAGE"; then
-        echo 600
-        return $ERR_LINK_TEMP_UNAVAILABLE
-    # {type:'download',url:'http://storXXXX.uploaded.to/dl/...'}
-    elif match 'url' "$PAGE"; then
-        FILE_URL=$(echo "$PAGE" | parse 'url' "url:'\(http.*\)'")
-    else
-        log_error "No match. Site update?"
+    ERR=$(echo "$JSON" | parse_json_quiet 'err')
+
+    if [ -n "$ERR" ]; then
+        if [ "$ERR" = 'captcha' ]; then
+            log_error 'Captcha wrong'
+            captcha_nack "$ID"
+            return $ERR_CAPTCHA
+        fi
+
+        captcha_ack "$ID"
+
+        # You have reached the max. number of possible free downloads for this hour
+        if match 'possible free downloads for this hour' "$ERR"; then
+            log_error 'Hourly limit reached.'
+            echo 3600
+            return $ERR_LINK_TEMP_UNAVAILABLE
+
+        # This file exceeds the max. filesize which can be downloaded by free users.
+        elif match 'exceeds the max. filesize' "$ERR"; then
+            return $ERR_SIZE_LIMIT_EXCEEDED
+
+        # We\'re sorry but all of our available download slots are busy currently
+        elif match 'all of our available download slots are busy' "$ERR"; then
+            log_error 'No free download slots available'
+            echo 300 # wait some arbitrary time
+            return $ERR_LINK_TEMP_UNAVAILABLE
+        fi
+
+        log_error "Unexpected remote error: $ERR"
         return $ERR_FATAL
     fi
 
-    captcha_ack $ID
-    log_debug "correct captcha"
+    captcha_ack "$ID"
 
+    # {type:'download',url:'http://storXXXX.uploaded.to/dl/...'}
+    # Note: This is no valid JSON due to the unquoted/single quoted strings
+    FILE_URL=$(echo "$JSON" | uploaded_to_parse_json_alt 'url') || return
     FILE_NAME=$(curl "$BASE_URL/file/$FILE_ID/status" | first_line) || return
-    if [ -z "$FILE_NAME" ]; then
-        # retrieve (truncated) filename
-        # Only 1 access to "final" URL is allowed, so we can't get complete name
-        # using "Content-Disposition:" header
-        FILE_NAME=$(echo "$HTML" | parse_tag_quiet 'id="filename"' 'a' | replace '&hellip;' '.')
-    fi
 
     echo "$FILE_URL"
     echo "$FILE_NAME"
