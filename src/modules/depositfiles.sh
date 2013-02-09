3d705fc7c8e5d009c8f0174247ab38fe65ccb03b
Jan <krompospeed@googlemail.com>
2013-02-09 22:52:31 +0100

    depositfiles: extend accepted domains, also accept HTTPS links

--- src/modules/depositfiles.sh
+++ src/modules/depositfiles.sh
@@ -18,7 +18,7 @@
 # You should have received a copy of the GNU General Public License
 # along with Plowshare.  If not, see <http://www.gnu.org/licenses/>.
 
-MODULE_DEPOSITFILES_REGEXP_URL="http://\(www\.\)\?\(depositfiles\.com\|dfiles\.eu\)/"
+MODULE_DEPOSITFILES_REGEXP_URL="https\?://\(www\.\)\?\(depositfiles\.\(com\|org\)\)\|\(dfiles\.\(eu\|ru\)\)/"
 
 MODULE_DEPOSITFILES_DOWNLOAD_OPTIONS="
 AUTH,a,auth,a=USER:PASSWORD,User account"
