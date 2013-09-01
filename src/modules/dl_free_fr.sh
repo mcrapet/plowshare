337b5eb2b30ac7dc104a3b1c4f475c96fafc7600
Matthieu Crapet <mcrapet@gmail.com>
2013-09-01 09:05:17 +0200

    dl.free.fr: minor, fix url match regexp

--- src/modules/dl_free_fr.sh
+++ src/modules/dl_free_fr.sh
@@ -18,7 +18,7 @@
 # You should have received a copy of the GNU General Public License
 # along with Plowshare.  If not, see <http://www.gnu.org/licenses/>.
 
-MODULE_DL_FREE_FR_REGEXP_URL="http://dl.free.fr/"
+MODULE_DL_FREE_FR_REGEXP_URL="http://dl\.free\.fr/"
 
 MODULE_DL_FREE_FR_DOWNLOAD_OPTIONS=""
 MODULE_DL_FREE_FR_DOWNLOAD_RESUME=yes
