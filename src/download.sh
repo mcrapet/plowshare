3db3032e1b46ab1b500908f2310fd9405b68fb30
Matthieu Crapet <mcrapet@gmail.com>
2013-10-19 18:30:02 +0200

    plowdown: add %s token (file size) to --printf switch
    This useful information is returned by final curl (-w '%{size_download}').
    
    + man page (plowdown.1) updated

--- src/download.sh
+++ src/download.sh
@@ -215,7 +215,7 @@ download() {
     local -r LAST_HOST=$8
 
     local DRETVAL DRESULT AWAIT FILE_NAME FILE_URL COOKIE_FILE COOKIE_JAR ANAME
-    local -i STATUS
+    local -i STATUS FILE_SIZE
     local URL_ENCODED=$(uri_encode <<< "$URL_RAW")
     local FUNCTION=${MODULE}_download
 
@@ -523,10 +523,11 @@ download() {
             fi
 
             DRETVAL=0
-            curl_with_log "${CURL_ARGS[@]}" -w '%{http_code}' --fail --globoff \
+            curl_with_log "${CURL_ARGS[@]}" --fail --globoff \
+                -w '%{http_code}\t%{size_download}' \
                 -o "$FILENAME_TMP" "$FILE_URL" >"$DRESULT" || DRETVAL=$?
 
-            read STATUS < "$DRESULT"
+            IFS=$'\t' read -r STATUS FILE_SIZE < "$DRESULT"
             rm -f "$DRESULT"
 
             if module_config_need_cookie "$MODULE"; then
@@ -599,7 +600,7 @@ download() {
 
         # Pretty print results
         local -a DATA=("$MODULE" "$FILE_NAME" "$OUT_DIR" "$COOKIE_JAR" \
-                    "$URL_ENCODED" "$FILE_URL")
+                    "$URL_ENCODED" "$FILE_URL" "$FILE_SIZE")
         pretty_print $INDEX DATA[@] "${PRINTF_FORMAT:-%F}"
 
         return 0
@@ -617,6 +618,7 @@ download() {
 # %f: destination (local) filename
 # %F: destination (local) filename (with output directory)
 # %m: module name
+# %s: destination (local) file size (in bytes)
 # %u: download (source) url
 # and also:
 # %n: newline
@@ -629,7 +631,7 @@ download() {
 pretty_check() {
     # This must be non greedy!
     local S TOKEN
-    S=${1//%[cdfmuCFnt%]}
+    S=${1//%[cdfmsuCFnt%]}
     TOKEN=$(parse_quiet . '\(%.\)' <<< "$S")
     if [ -n "$TOKEN" ]; then
         log_error "Bad format string: unknown sequence << $TOKEN >>"
@@ -638,7 +640,7 @@ pretty_check() {
 }
 
 # $1: unique number
-# $2: array[@] (module, dfile, ddir, cdata, dls, dlf)
+# $2: array[@] (module, filename, out_dir, cookies, dl_url, final_url, filesize)
 # $3: format string
 # Note: Don't chmod cookie file (keep strict permissions)
 pretty_print() {
@@ -647,6 +649,7 @@ pretty_print() {
     local FMT=$3
     local COOKIE_FILE
 
+    test "${FMT#*%s}" != "$FMT" && FMT=$(replace '%s' "${A[6]}" <<< "$FMT")
     test "${FMT#*%m}" != "$FMT" && FMT=$(replace '%m' "${A[0]}" <<< "$FMT")
     test "${FMT#*%f}" != "$FMT" && FMT=$(replace '%f' "${A[1]}" <<< "$FMT")
 
