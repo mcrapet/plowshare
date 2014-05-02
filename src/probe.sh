mcrapet/plowshare

commit d9e346472ba66ed30f269214cd997da6de3c67d7

core.sh (process_configfile_*): enhance config file management
Changes:
- first letter of section name can be lowercase
- section order in plowshare.conf is no longer relevant
- don't use grep anymore (sed still used)
- leading blank before module option is now accepted
- man page (plowshare.conf.5): tiny update

src/probe.sh
@@ -255,7 +255,7 @@ done
 
 # Get configuration file options. Command-line is not parsed yet.
 match '--no-plowsharerc' "$*" || \
-    process_configfile_options 'Plowprobe' "$OPTIONS"
+    process_configfile_options '[Pp]lowprobe' "$OPTIONS"
 
 # Process plowprobe options
 eval "$(process_core_options 'plowprobe' "$OPTIONS" "$@")" || exit
@@ -361,7 +361,7 @@ for ITEM in "${COMMAND_LINE_ARGS[@]}"; do
         else
             # Get configuration file module options (yes, Plowdown's one)
             test -z "$NO_PLOWSHARERC" && \
-                process_configfile_module_options 'Plowdown' "$MODULE" PROBE
+                process_configfile_module_options '[Pp]lowprobe' "$MODULE" PROBE
 
             eval "$(process_module_options "$MODULE" PROBE \
                 "${COMMAND_LINE_MODULE_OPTS[@]}")" || true

docs/plowshare.conf.5
@@ -23,7 +23,7 @@
 .\"   - xorg.conf.5
 .\"   - sysctl.conf.5
 
-.TH "plowshare.conf" "5" "January 24, 2013" "GPL" "Plowshare for Bash 4, GIT-snapshot"
+.TH "plowshare.conf" "5" "January 25, 2013" "GPL" "Plowshare for Bash 4, GIT-snapshot"
 
 .SH NAME
 plowshare.conf \- Configuration file for Plowshare
@@ -71,6 +71,8 @@ file uses an INI-style format. It is composed of six sections which may be prese
 .PP
 Notes:
 .IP .
+Sections are unordered.
+.IP .
 Blank lines or lines beginning with a hash character ('#') are treated as comments and ignored.
 .IP .
 Whitespace before and after a \fItoken\fP or \fIvalue\fP is ignored (although a \fIvalue\fP can contain whitespace within).
@@ -78,10 +80,10 @@ Whitespace before and after a \fItoken\fP or \fIvalue\fP is ignored (although a 
 Double quoting \fIvalue\fP is optional.
 .PP
 Valid tokens are always lowercase.
-Names are long-option command-line arguments of plowshare. For example: \fItemp-directory\fP.
+Names are long-option command-line arguments of plowshare. For example: \fItemp-directory\fP for plowdown.
 For modules options, tokens are prepended by module name and a slash character. For example: \fIhotfile/auth\fP (short-option are also possible here).
 
-For values, no backslash interpretations is made. "\\t" is a regular string with two characters, it is not a tab character.
+For values, no backslash interpretation is made. "\\t" is a regular string with two characters, it is not a tab character.
 
 Options in general section prevail over Plow* section. Options given on the command line prevail over configuration file options.
 

src/core.sh
@@ -2198,13 +2198,17 @@ process_configfile_options() {
     # Strip spaces in options
     OPTIONS=$(strip_and_drop_empty_lines "$2")
 
-    SECTION=$(sed -ne "/\[$1\]/,/^\[/p" -ne "/\[General\]/,/^\[/p" "$CONFIG" | \
-              sed -e '/^\(#\|\[\|[[:space:]]*$\)/d')
+    # [General] section before [$1] section
+    SECTION=$(sed -ne "/\[$1\]/,/^\[/H; /\[[Gg]eneral\]/,/^\[/p; \${x;p}" \
+        "$CONFIG" | sed -e '/^\(#\|\[\|[[:space:]]*$\)/d') || true
 
     if [ -n "$SECTION" -a -n "$OPTIONS" ]; then
         while read -r LINE; do
             NAME=$(strip <<< "${LINE%%=*}")
             VALUE=$(strip <<< "${LINE#*=}")
+
+            # If NAME contain a '/' character, this is a module option, skip it
+            [[ $NAME = */* ]] && continue
 
             # Look for optional double quote (protect leading/trailing spaces)
             if [ '"' = "${VALUE:0:1}" -a '"' = "${VALUE:(-1):1}" ]; then
@@ -2213,7 +2217,7 @@ process_configfile_options() {
             fi
 
             # Look for 'long_name' in options list
-            OPTION=$(echo "$OPTIONS" | grep ",${NAME}:\?," | sed '1q') || true
+            OPTION=$(sed -ne "/,.\?,${NAME},/{p;q}" <<< "$OPTIONS") || true
             if [ -n "$OPTION" ]; then
                 local VAR=${OPTION%%,*}
                 eval "$VAR=\$VALUE"
@@ -2248,8 +2252,9 @@ process_configfile_module_options() {
 
     OPTIONS=$(get_module_options "$2" "$3")
 
-    SECTION=$(sed -ne "/\[$1\]/,/^\[/p" -ne "/\[General\]/,/^\[/p" "$CONFIG" | \
-              sed -e '/^\(#\|\[\|[[:space:]]*$\)/d')
+    # [General] section after [$1] section
+    SECTION=$(sed -ne "/\[[Gg]eneral\]/,/^\[/H; /\[$1\]/,/^\[/p; \${x;p}" \
+        "$CONFIG" | sed -e '/^\(#\|\[\|[[:space:]]*$\)/d') || true
 
     if [ -n "$SECTION" -a -n "$OPTIONS" ]; then
         local -r M=$(lowercase "$2")
@@ -2260,7 +2265,7 @@ process_configfile_module_options() {
             IFS="," read VAR SHORT LONG TYPE_HELP <<< "$REPLY"
 
             # Look for 'module/option_name' (short or long) in section list
-            LINE=$(echo "$SECTION" | grep "^$M/\($SHORT\|$LONG\)[[:space:]]*=" | sed -n '$p') || true
+            LINE=$(sed -ne "/^[[:space:]]*$M\/\($SHORT\|$LONG\)[[:space:]]*=/{p;q}" <<< "$SECTION") || true
             if [ -n "$LINE" ]; then
                 VALUE=$(strip <<< "${LINE#*=}")
 

src/delete.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #
 # Delete a file from file sharing servers
-# Copyright (c) 2010-2012 Plowshare team
+# Copyright (c) 2010-2013 Plowshare team
 #
 # This file is part of Plowshare.
 #
@@ -85,7 +85,7 @@ done
 
 # Get configuration file options. Command-line is not parsed yet.
 match '--no-plowsharerc' "$*" || \
-    process_configfile_options 'Plowdel' "$OPTIONS"
+    process_configfile_options '[Pp]lowdel' "$OPTIONS"
 
 # Process plowup options
 eval "$(process_core_options 'plowdel' "$OPTIONS" "$@")" || exit
@@ -152,7 +152,7 @@ for URL in "${COMMAND_LINE_ARGS[@]}"; do
 
     # Get configuration file module options
     test -z "$NO_PLOWSHARERC" && \
-        process_configfile_module_options 'Plowdel' "$MODULE" DELETE
+        process_configfile_module_options '[Pp]lowdel' "$MODULE" DELETE
 
     eval "$(process_module_options "$MODULE" DELETE \
         "${COMMAND_LINE_MODULE_OPTS[@]}")" || true

src/download.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #
 # Download files from file sharing servers
-# Copyright (c) 2010-2012 Plowshare team
+# Copyright (c) 2010-2013 Plowshare team
 #
 # Output filenames are printed on standard output (one per line).
 #
@@ -677,7 +677,7 @@ done
 
 # Get configuration file options. Command-line is not parsed yet.
 match '--no-plowsharerc' "$*" || \
-    process_configfile_options 'Plowdown' "$OPTIONS"
+    process_configfile_options '[Pp]lowdown' "$OPTIONS"
 
 # Process plowdown options
 eval "$(process_core_options 'plowdown' "$OPTIONS" "$@")" || exit
@@ -859,7 +859,7 @@ for ITEM in "${COMMAND_LINE_ARGS[@]}"; do
         else
             # Get configuration file module options
             test -z "$NO_PLOWSHARERC" && \
-                process_configfile_module_options 'Plowdown' "$MODULE" DOWNLOAD
+                process_configfile_module_options '[Pp]lowdown' "$MODULE" DOWNLOAD
 
             eval "$(process_module_options "$MODULE" DOWNLOAD \
                 "${COMMAND_LINE_MODULE_OPTS[@]}")" || true

src/list.sh
@@ -173,7 +173,7 @@ done
 
 # Get configuration file options. Command-line is not parsed yet.
 match '--no-plowsharerc' "$*" || \
-    process_configfile_options 'Plowlist' "$OPTIONS"
+    process_configfile_options '[Pp]lowlist' "$OPTIONS"
 
 # Process plowup options
 eval "$(process_core_options 'plowlist' "$OPTIONS" "$@")" || exit
@@ -261,7 +261,7 @@ for URL in "${COMMAND_LINE_ARGS[@]}"; do
 
     # Get configuration file module options
     test -z "$NO_PLOWSHARERC" && \
-        process_configfile_module_options 'Plowlist' "$MODULE" LIST
+        process_configfile_module_options '[Pp]lowlist' "$MODULE" LIST
 
     eval "$(process_module_options "$MODULE" LIST \
         "${COMMAND_LINE_MODULE_OPTS[@]}")" || true

src/upload.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #
 # Upload a file to file sharing servers
-# Copyright (c) 2010-2012 Plowshare team
+# Copyright (c) 2010-2013 Plowshare team
 #
 # Output URL to standard output.
 #
@@ -199,7 +199,7 @@ done
 
 # Get configuration file options. Command-line is not parsed yet.
 match '--no-plowsharerc' "$*" || \
-    process_configfile_options 'Plowup' "$OPTIONS"
+    process_configfile_options '[Pp]lowup' "$OPTIONS"
 
 # Process plowup options
 eval "$(process_core_options 'plowup' "$OPTIONS" "$@")" || exit
@@ -271,7 +271,7 @@ fi
 
 # Get configuration file module options
 test -z "$NO_PLOWSHARERC" && \
-    process_configfile_module_options 'Plowup' "$MODULE" UPLOAD
+    process_configfile_module_options '[Pp]lowup' "$MODULE" UPLOAD
 
 eval "$(process_module_options "$MODULE" UPLOAD \
     "${COMMAND_LINE_MODULE_OPTS[@]}")" || true

commit 3ef402b291146cc168170a4bd46580aa7f9b7d26

plowprobe: comment fix
Plowprobe has its own section in configuration file.
Bug introduced in revision d9e346472ba66ed30f269214cd997da6de3c67d7

<diff>
-                process_configfile_module_options 'Plowdown' "$MODULE" PROBE
+                process_configfile_module_options '[Pp]lowprobe' "$MODULE" PROBE
</diff>

src/probe.sh
@@ -410,7 +410,7 @@ for ITEM in "${COMMAND_LINE_ARGS[@]}"; do
             RETVALS+=(0)
             echo "$MODULE"
         else
-            # Get configuration file module options (yes, Plowdown's one)
+            # Get configuration file module options
             test -z "$NO_PLOWSHARERC" && \
                 process_configfile_module_options '[Pp]lowprobe' "$MODULE" PROBE "$EXT_PLOWSHARERC"
 