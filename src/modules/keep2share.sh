909d28cfbd3cc121418f6fae1a1336f1df9ff2ac
Jan <krompospeed@googlemail.com>
2014-07-24 13:50:47 +0200

    keep2share (download): handle captcha and forced delay
    Also:
    - get canonical URL explicitly
    - fortify code ('readonly')
    - enable resuming of downloads

--- src/modules/keep2share.sh
+++ src/modules/keep2share.sh
@@ -23,7 +23,7 @@
 MODULE_KEEP2SHARE_REGEXP_URL='https\?://\(www\.\)\?\(keep2share\|k2s\|k2share\|keep2s\)\.cc/'
 
 MODULE_KEEP2SHARE_DOWNLOAD_OPTIONS=""
-MODULE_KEEP2SHARE_DOWNLOAD_RESUME=no
+MODULE_KEEP2SHARE_DOWNLOAD_RESUME=yes
 MODULE_KEEP2SHARE_DOWNLOAD_FINAL_LINK_NEEDS_COOKIE=no
 
 MODULE_KEEP2SHARE_UPLOAD_OPTIONS="
@@ -75,11 +75,16 @@ keep2share_login() {
 # stdout: real file download link
 keep2share_download() {
     local -r COOKIE_FILE=$1
-    local -r URL=$2
-    local -r BASE_URL='http://k2s.cc'
-    local PAGE FORM_HTML FORM_ID FORM_ACTION FILE_NAME PRE_URL
+    local URL BASE_URL FILE_NAME PRE_URL
+    local PAGE FORM_HTML FORM_ID FORM_ACTION WAIT
 
-    PAGE=$(curl --location -c "$COOKIE_FILE" "$URL") || return
+    # get canonical URL and BASE_URL for this file
+    URL=$(curl -I "$2" | grep_http_header_location_quiet) || return
+    [ -n "$URL" ] || URL=$2
+    BASE_URL=${URL%/file*}
+    readonly URL BASE_URL
+
+    PAGE=$(curl -c "$COOKIE_FILE" "$URL") || return
 
     # File not found or deleted
     if match 'File not found or deleted' "$PAGE"; then
@@ -88,6 +93,19 @@ keep2share_download() {
 
     FILE_NAME=$(parse_tag '^[[:space:]]*File:' 'span' <<< "$PAGE" | \
         html_to_utf8) || return
+    readonly FILE_NAME
+
+    # check for pre-unlocked link
+    # <span id="temp-link">To download this file with slow speed, use <a href="/file/url.html?file=abcdefghi">this link</a><br>
+    if match 'temp-link' "$PAGE"; then
+        PRE_URL=$(parse_attr 'temp-link' 'href' <<< "$PAGE") || return
+        PAGE=$(curl --head -b "$COOKIE_FILE" "$BASE_URL$PRE_URL") || return
+
+        # output final url + file name
+        grep_http_header_location <<< "$PAGE" || return
+        echo "$FILE_NAME"
+        return 0
+    fi
 
     FORM_HTML=$(grep_form_by_order "$PAGE") || return
     FORM_ID=$(parse_form_input_by_name 'slow_id' <<< "$FORM_HTML") || return
@@ -97,20 +115,103 @@ keep2share_download() {
     PAGE=$(curl -b "$COOKIE_FILE" -d "slow_id=$FORM_ID" -d 'yt0' \
         "$BASE_URL$FORM_ACTION") || return
 
-    # check for CAPTCHA
-    if match 'id="captcha-form"' "$PAGE"; then
-        log_error 'Captcha handling not implemented yet'
-        return $ERR_FATAL
+    # check for forced delay
+    WAIT=$(parse_quiet 'Please wait .* to download this file' \
+        'wait \([[:digit:]:]\+\) to download' <<< "$PAGE")
+
+    if [ -n "$WAIT" ]; then
+        local HOUR MIN SEC
+
+        HOUR=${WAIT%%:*}
+        SEC=${WAIT##*:}
+        MIN=${WAIT#*:}; MIN=${MIN%:*}
+
+        log_error 'Forced delay between downloads.'
+        echo $(( (( HOUR * 60 ) + MIN ) * 60 + SEC ))
+        return $ERR_LINK_TEMP_UNAVAILABLE
     fi
 
-    if ! match 'btn-success.*Download' "$PAGE"; then
+    # check for and handle CAPTCHA (if any)
+    # Note: emulate 'grep_form_by_id_quiet'
+    FORM_HTML=$(grep_form_by_id "$PAGE" 'captcha-form' 2>/dev/null)
+
+    if [ -n "$FORM_HTML" ]; then
+        local RESP WORD ID CAPTCHA_DATA
+
+        if match 'recaptcha' "$FORM_HTML"; then
+            log_debug 'reCaptcha found'
+            local CHALL
+            local -r PUBKEY='6LcYcN0SAAAAABtMlxKj7X0hRxOY8_2U86kI1vbb'
+
+            RESP=$(recaptcha_process $PUBKEY) || return
+            { read WORD; read CHALL; read ID; } <<< "$RESP"
+
+            CAPTCHA_DATA="-d CaptchaForm%5Bcode%5D -d recaptcha_challenge_field=$CHALL --data-urlencode recaptcha_response_field=$WORD"
+
+        elif match 'captcha.html' "$FORM_HTML"; then
+            log_debug 'Captcha found'
+            local CAPTCHA_URL IMG_FILE
+
+            # Get captcha image
+            CAPTCHA_URL=$(parse_attr '<img' 'src' <<< "$FORM_HTML") || return
+            IMG_FILE=$(create_tempfile '.keep2share.png') || return
+            curl -b "$COOKIE_FILE" -o "$IMG_FILE" "$BASE_URL$CAPTCHA_URL" || return
+
+            # Solve captcha
+            # Note: Image is a 260x80 png file containing 6-7 characters
+            RESP=$(captcha_process "$IMG_FILE" keep2share 6 7) || return
+            { read WORD; read ID; } <<< "$RESP"
+            rm -f "$IMG_FILE"
+
+            CAPTCHA_DATA="-d CaptchaForm%5Bcode%5D=$WORD"
+
+        else
+            log_error 'Unexpected content/captcha type. Site updated?'
+            return $ERR_FATAL
+        fi
+
+        log_debug "Captcha data: $CAPTCHA_DATA"
+
+        FORM_ID=$(parse_form_input_by_name 'uniqueId' <<< "$FORM_HTML") || return
+        FORM_ACTION=$(parse_form_action <<< "$FORM_HTML") || return
+
+        PAGE=$(curl -b "$COOKIE_FILE" $CAPTCHA_DATA \
+            -d 'free=1' -d 'freeDownloadRequest=1' \
+            -d "uniqueId=$FORM_ID" \
+            -d 'yt0' "$BASE_URL$FORM_ACTION") || return
+
+        if match 'The verification code is incorrect' "$PAGE"; then
+            log_error 'Wrong captcha'
+            captcha_nack "$ID"
+            return $ERR_CAPTCHA
+        fi
+
+        log_debug 'Correct captcha'
+        captcha_ack "$ID"
+
+        # parse wait time
+        WAIT=$(parse 'id="download-wait-timer"' \
+            '^[[:space:]]*\([[:digit:]]\+\)' 1 <<< "$PAGE") || return
+
+        if [ -n "$WAIT" ]; then
+            wait $(( WAIT + 1 )) || return
+        fi
+
+        PAGE=$(curl -b "$COOKIE_FILE" -d "uniqueId=$FORM_ID" \
+            -d 'free=1' "$BASE_URL$FORM_ACTION") || return
+
+        PRE_URL=$(parse_attr 'id="temp-link"' 'href' <<< "$PAGE") || return
+
+    # direct download without captcha
+    elif match 'btn-success.*Download' "$PAGE"; then
+        PRE_URL=$(parse 'window.location.href' "'\([^']\+\)'" <<< "$PAGE") || return
+    else
         log_error 'Unexpected content. Site updated?'
         return $ERR_FATAL
     fi
 
-    # get (redirecting) file URL
-    PRE_URL=$(parse 'window.location.href' "'\([^']\+\)'" <<< "$PAGE") || return
-    PAGE=$(curl --head -b "$COOKIE_FILE" "$BASE_URL/$PRE_URL") || return
+    log_debug "Pre-URL: '$PRE_URL'"
+    PAGE=$(curl --head -b "$COOKIE_FILE" "$BASE_URL$PRE_URL") || return
 
     # output final url + file name
     grep_http_header_location <<< "$PAGE" || return
