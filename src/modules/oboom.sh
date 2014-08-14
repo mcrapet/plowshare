d4646f4dd99199af1c4db747bc9fe92c2774b935
Matthieu Crapet <mcrapet@gmail.com>
2014-08-14 06:04:16 +0200

    oboom: fix login
    Fixes issue 1181.

--- src/modules/oboom.sh
+++ src/modules/oboom.sh
@@ -431,7 +431,7 @@ oboom_login() {
     PAGE=$(curl \
         -d "auth=$USER" \
         -d "pass=$PASS_PBKDF2" \
-        'http://www.oboom.com/1/login') || return
+        'https://www.oboom.com/1/login') || return
 
     oboom_check_error "$PAGE" 'Login' || return
 
